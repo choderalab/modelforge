343c27781721634e90f86308920c842de6d6ba66
wiederm <marcus.wieder@univie.ac.at>
2023-08-04 22:00:23 +0200

    updating based on Chris' comments and review

--- modelforge/dataset/dataset.py
+++ modelforge/dataset/dataset.py
@@ -4,6 +4,7 @@ from abc import ABC, abstractmethod
 import qcportal as ptl
 import torch
 from loguru import logger
+from typing import Tuple
 
 
 class Dataset(torch.utils.data.Dataset, ABC):
@@ -11,14 +12,16 @@ class Dataset(torch.utils.data.Dataset, ABC):
     Abstract Base Class representing a dataset.
     """
 
-    def __init__(self, dataset_file: str = None, name: str = ""):
+    def __init__(self, dataset_file: str = ""):
         self.dataset_file = dataset_file
-        self.name = name
         self.dataset = self.load(dataset_file)
         (
             self.molecules,
             self.records,
         ) = self.dataset.get_molecules(), self.dataset.get_records(method="b3lyp")
+
+    @abstractmethod
+    def 
 
     @property
     @abstractmethod
@@ -52,14 +55,17 @@ class Dataset(torch.utils.data.Dataset, ABC):
             if not dataset_file.endswith(".hdf5"):
                 raise ValueError("Input file must be an .hdf5 file.")
             logger.debug(f"Loading from {dataset_file}")
-            dataset.set_view(dataset_file)  # NOTE: this doesn't work yet
+            dataset.set_view(dataset_file)  
         else:
             logger.debug(f"Downloading from qcportal")
+            dataset.download(dataset_file)
 
-            # to get QM9 from qcportal, we need to define which collection and QM9
+            # If dataset_file was specified, but does not exist, the file will be downloaded from qcarchive and saved to the file/path specified by dataset_file
             if dataset_file is None:
                 dataset.download()
             else:
+                if not dataset_file.endswith(".hdf5"):
+                      raise ValueError("File must be an .hdf5 file.")
                 dataset.download(dataset_file)
                 dataset.to_file(path=dataset_file, encoding="hdf5")
         return dataset
@@ -67,7 +73,15 @@ class Dataset(torch.utils.data.Dataset, ABC):
     def __len__(self):
         return self.molecules.shape[0]
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx) -> Tuple[torch.Tensor, torch.Tensor]: # Tuple[0] with dim=3, Tuple[1] with dim=1
+        """ pytorch dataset getitem method to return a tuple of geometry and energy
+
+        Args:
+            idx (int): _description_
+
+        Returns:
+            Tuple[torch.Tensor, torch.Tensor]: returns a tuple of geometry and energy
+        """
         geometry = torch.tensor(self.records.iloc[idx].record["geometry"])
         energy = torch.tensor(self.records.iloc[idx].record["energy"])
 

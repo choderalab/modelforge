be235fe15dda4cfd68cac15ff21cd3b3aa25e33f
wiederm <marcus.wieder@univie.ac.at>
2023-08-04 22:39:51 +0200

    suggestion for including dataset properties and enable users to define them

--- modelforge/dataset/dataset.py
+++ modelforge/dataset/dataset.py
@@ -12,16 +12,30 @@ class Dataset(torch.utils.data.Dataset, ABC):
     Abstract Base Class representing a dataset.
     """
 
-    def __init__(self, dataset_file: str = ""):
+    def __init__(
+        self,
+        dataset_file: str = "",
+    ):
         self.dataset_file = dataset_file
         self.dataset = self.load(dataset_file)
+        records_properties = self._dataset_records
         (
             self.molecules,
             self.records,
-        ) = self.dataset.get_molecules(), self.dataset.get_records(method="b3lyp")
+        ) = self.dataset.get_molecules(), self.dataset.get_records(
+            method=records_properties["method"],
+            basis=records_properties["basis"],
+            program=records_properties["program"],
+        )
 
     @abstractmethod
-    def 
+    @property
+    def _dataset_records(self) -> dict:
+        """
+        Defines the dataset to be downloaded.
+        Three keys are required: 'method', 'basis', 'program'.
+        """
+        pass
 
     @property
     @abstractmethod
@@ -55,7 +69,7 @@ class Dataset(torch.utils.data.Dataset, ABC):
             if not dataset_file.endswith(".hdf5"):
                 raise ValueError("Input file must be an .hdf5 file.")
             logger.debug(f"Loading from {dataset_file}")
-            dataset.set_view(dataset_file)  
+            dataset.set_view(dataset_file)
         else:
             logger.debug(f"Downloading from qcportal")
             dataset.download(dataset_file)
@@ -72,8 +86,10 @@ class Dataset(torch.utils.data.Dataset, ABC):
     def __len__(self):
         return self.molecules.shape[0]
 
-    def __getitem__(self, idx) -> Tuple[torch.Tensor, torch.Tensor]: # Tuple[0] with dim=3, Tuple[1] with dim=1
-        """ pytorch dataset getitem method to return a tuple of geometry and energy
+    def __getitem__(
+        self, idx
+    ) -> Tuple[torch.Tensor, torch.Tensor]:  # Tuple[0] with dim=3, Tuple[1] with dim=1
+        """pytorch dataset getitem method to return a tuple of geometry and energy
 
         Args:
             idx (int): _description_
@@ -104,3 +120,11 @@ class QM9Dataset(Dataset):
     @property
     def name(self):
         return "QM9"
+
+    @property
+    def _dataset_records():
+        return {
+            "method": "b3lyp",
+            "basis": "6-31g",
+            "program": "psi4",
+        }

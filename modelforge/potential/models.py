6261a03a6c13cba70ab402f0a78d83ef45ff1de9
wiederm <marcus.wieder@gmail.com>
2023-08-29 12:17:22 +0200

    simplify base model

--- modelforge/potential/models.py
+++ modelforge/potential/models.py
@@ -2,110 +2,14 @@ import torch.nn as nn
 import torch
 from typing import Dict, List, Optional
 
+from modelforge.utils import Properties, Inputs, SpeciesEnergies
 
-class Transform(nn.Module):
-    pass
-
-
-class PotentialBase(nn.Module):
-    """
-    Base class for all atomistic models.
-
-    Parameters
-    ----------
-    input_dtype : str, optional
-        Data type of real inputs, default is "float32".
-
-    Attributes
-    ----------
-    required_derivatives : Optional[List[str]]
-        List of required derivatives.
-    model_outputs : Optional[List[str]]
-        List of model outputs.
-    """
-
-    def __init__(
-        self,
-        input_dtype: str = "float32",
-    ):
-        super().__init__()
-        self.input_dtype = input_dtype
-        self.required_derivatives: Optional[List[str]] = None
-        self.model_outputs: Optional[List[str]] = None
-
-    def collect_derivatives(self) -> List[str]:
-        """
-        Collect required derivatives from submodules.
-
-        Returns
-        -------
-        List[str]
-            List of required derivatives.
-        """
-        # self.required_derivatives = None
-        required_derivatives = set()
-        for m in self.modules():
-            if (
-                hasattr(m, "required_derivatives")
-                and m.required_derivatives is not None
-            ):
-                required_derivatives.update(m.required_derivatives)
-        required_derivatives: List[str] = list(required_derivatives)
-        self.required_derivatives = required_derivatives
-
-    def collect_outputs(self) -> List[str]:
-        """
-        Collect model outputs from submodules.
-
-        Returns
-        -------
-        List[str]
-            List of model outputs.
-        """
-        # self.model_outputs = None
-        
-        model_outputs = set()
-        for m in self.modules():
-            if hasattr(m, "model_outputs") and m.model_outputs is not None:
-                model_outputs.update(m.model_outputs)
-        model_outputs: List[str] = list(model_outputs)
-        self.model_outputs = model_outputs
-
-    def initialize_derivatives(
-        self, inputs: Dict[str, torch.Tensor]
-    ) -> Dict[str, torch.Tensor]:
-        """
-        Initialize derivatives for response properties.
-
-        Parameters
-        ----------
-        inputs : Dict[str, torch.Tensor]
-            Dictionary of input tensors.
-
-        Returns
-        -------
-        Dict[str, torch.Tensor]
-            Dictionary of input tensors with required gradients enabled.
-        """
-
-        for p in self.required_derivatives:
-            if p in inputs.keys():
-                inputs[p].requires_grad_()  # set in place
-        return inputs
-
-    def extract_outputs(
-        self, inputs: Dict[str, torch.Tensor]
-    ) -> Dict[str, torch.Tensor]:
-        results = {k: inputs[k] for k in self.model_outputs}
-        return results
+import torch.nn as nn
+import torch
+from typing import Dict, List, Optional
 
 
-class NeuralNetworkPotential(PotentialBase):
-    """
-    A generic neural network potential class that sequentially applies a list of input
-    modules, a representation module, and a list of output modules.
-    """
-
+class BaseNNP(nn.Module):
     def __init__(
         self,
         representation: nn.Module,
@@ -134,33 +38,12 @@ class NeuralNetworkPotential(PotentialBase):
         self.input_modules = nn.ModuleList(input_modules or [])
         self.output_modules = nn.ModuleList(output_modules or [])
 
-        self.collect_derivatives()
-        self.collect_outputs()
+    def forward(
+        self,
+        inputs: Inputs,
+    ) -> SpeciesEnergies:
+        E = self.calculate_energies_and_forces(inputs)
+        return SpeciesEnergies(inputs.Z, E)
 
-    def forward(self, inputs: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
-        """
-        Forward pass for the neural network potential.
-
-        Parameters
-        ----------
-        inputs : Dict[str, torch.Tensor]
-            Dictionary of input tensors.
-
-        Returns
-        -------
-        Dict[str, torch.Tensor]
-            Dictionary of output tensors.
-        """
-        inputs = self.initialize_derivatives(inputs)
-
-        for m in self.input_modules:
-            inputs = m(inputs)
-
-        inputs = self.representation(inputs)
-
-        for m in self.output_modules:
-            inputs = m(inputs)
-
-        results = self.extract_outputs(inputs)
-
-        return results
+    def calculate_energies_and_forces():
+        raise NotImplementedError
---
+++ modelforge/utils/prop.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+import torch
+from typing import Optional
+
+
+@dataclass
+class Properties:
+    Z: str = "atomic_numbers"
+    R: str = "positions"
+
+
+@dataclass
+class Inputs:
+    Z: torch.Tensor
+    R: torch.Tensor
+    cell: Optional[torch.Tensor] = (None,)
+    pbc: Optional[torch.Tensor] = (None,)
+
+    def __post_init__(self):
+        assert self.Z.shape[0] == self.R.shape[0]
+
+
+@dataclass
+class AtomicEnergies:
+    species: torch.Tensor
+    energies: torch.Tensor

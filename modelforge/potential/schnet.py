8e373352885a3c21c9f54942301af8bc47a5c6cc
wiederm <marcus.wieder@gmail.com>
2023-09-25 15:53:16 +0200

    split representation and interaction block

--- modelforge/potential/schnet.py
+++ modelforge/potential/schnet.py
@@ -49,6 +49,13 @@ class Schnet(BaseNNP):
         self.readout = EnergyReadout(n_atom_basis)
         self.embedding = nn.Embedding(100, n_atom_basis, padding_idx=-1)
 
+        self.interactions = nn.ModuleList(
+            [
+                SchNetInteractionBlock(n_atom_basis, n_filters)
+                for _ in range(n_interactions)
+            ]
+        )
+
     def calculate_energy(
         self, inputs: Dict[str, torch.Tensor], cached_pairlist: bool = False
     ) -> torch.Tensor:
@@ -77,8 +84,19 @@ class Schnet(BaseNNP):
         mask = Z == -1
         pairlist = self.calculate_distances_and_pairlist(mask, inputs["R"])
 
-        x = self.representation(x, pairlist)
-        # pool average over atoms
+        representation = self.representation(x, pairlist)
+        for interaction in self.interactions:
+            v = interaction(
+                representation["f_ij"],
+                representation["idx_i"],
+                representation["idx_j"],
+                representation["rcut_ij"],
+            )
+            x = x + v
+
+        # x with shape torch.Tensor, shape [batch_size, n_atoms, n_atom_basis]
+
+        # pool average over atoms # TODO: check if is this correct?
         return self.readout(x)
 
 
@@ -194,12 +212,6 @@ class SchNetRepresentation(nn.Module):
         """
         super().__init__()
 
-        self.interactions = nn.ModuleList(
-            [
-                SchNetInteractionBlock(n_atom_basis, n_filters)
-                for _ in range(n_interactions)
-            ]
-        )
         self.cutoff = 5.0
         self.radial_basis = GaussianRBF(n_rbf=20, cutoff=self.cutoff)
 
@@ -224,9 +236,7 @@ class SchNetRepresentation(nn.Module):
         rcut_ij = cosine_cutoff(d_ij, self.cutoff)
         return f_ij, rcut_ij
 
-    def forward(
-        self, x: torch.Tensor, pairlist: Dict[str, torch.Tensor]
-    ) -> torch.Tensor:
+    def forward(self, pairlist: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
         """
         Forward pass for the representation layer.
 
@@ -242,17 +252,23 @@ class SchNetRepresentation(nn.Module):
                 Pairwise distances between atoms.
         Returns
         -------
-        torch.Tensor, shape [batch_size, n_atoms, n_atom_basis]
-            Output tensor after forward pass.
+        Dict[str, torch.Tensor]
+            Dictionary containing the following keys:
+            - 'f_ij': torch.Tensor, shape [n_pairs, n_rbf]
+                Radial basis functions for pairs of atoms.
+            - 'idx_i': torch.Tensor, shape [n_pairs]
+                Indices for the first atom in each pair.
+            - 'idx_j': torch.Tensor, shape [n_pairs]
+                Indices for the second atom in each pair.
+            - 'rcut_ij': torch.Tensor, shape [n_pairs]
+                Cutoff values for each pair.
         """
+
         atom_index12 = pairlist["atom_index12"]
         d_ij = pairlist["d_ij"]
 
         f_ij, rcut_ij = self._distance_to_radial_basis(d_ij)
 
         idx_i, idx_j = atom_index12[0], atom_index12[1]
-        for interaction in self.interactions:
-            v = interaction(x, f_ij, idx_i, idx_j, rcut_ij)
-            x = x + v
 
-        return x
+        return {"f_ij": f_ij, "idx_i": idx_i, "idx_j": idx_j, "rcut_ij": rcut_ij}

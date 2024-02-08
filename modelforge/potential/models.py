Fix loss calculation and initialize filter networks

modelforge/potential/models.py
@@ -186,9 +186,7 @@ class LightningModuleMixin(pl.LightningModule):
         targets = batch["E_label"].flatten().to(torch.float32)
         loss = self.loss_function(predictions, targets)
 
-        if self.report_loss_in_kJ_per_mol and not self.energy_average:
-            log.warning("Please provide the energy average")
-        else:
+        if self.report_loss_in_kJ_per_mol and self.energy_average:
             if self.energy_stddev:
                 loss = loss * self.energy_stddev
 

modelforge/potential/painn.py
@@ -69,17 +69,14 @@ class PaiNN(BaseNNP):
 
         # initialize the filter network
         if shared_filters:
-            self.filter_net = nn.Sequential(
-                nn.Linear(self.radial_basis_module.n_rbf, 3 * self.nr_atom_basis),
-                nn.Identity(),
+            self.filter_net = Dense(
+                self.radial_basis_module.n_rbf, 3 * self.nr_atom_basis
             )
         else:
-            self.filter_net = nn.Sequential(
-                nn.Linear(
-                    self.radial_basis_module.n_rbf,
-                    self.nr_interaction_blocks * 3 * self.nr_atom_basis,
-                ),
-                nn.Identity(),
+            self.filter_net = Dense(
+                self.radial_basis_module.n_rbf,
+                self.nr_interaction_blocks * self.nr_atom_basis * 3,
+                activation=None,
             )
 
         # initialize the interaction and mixing networks

modelforge/potential/schnet.py
@@ -203,7 +203,7 @@ class SchNETInteractionBlock(nn.Module):
 
         # Initialize a tensor to gather the results
         shape = list(x.shape)  # note that we're using x.shape, not x_ij.shape
-        x_native = torch.zeros(shape, dtype=x.dtype)
+        x_native = torch.zeros(shape, dtype=x.dtype, device=x.device)
 
         idx_i_expanded = idx_i.unsqueeze(1).expand_as(x_ij)
 
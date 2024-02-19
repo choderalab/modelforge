3ad5e1420a0145a3ff92014a17f5d831af511b57
wiederm <marcus.wieder@gmail.com>
2024-02-19 19:10:06 +0000

    Add tests for comparing angular symmetry features

--- modelforge/tests/test_ani.py
+++ modelforge/tests/test_ani.py
@@ -37,7 +37,6 @@ def test_ani(setup_methane):
     force = -derivative
 
 
-
 def test_compare_radial_symmetry_features():
 
     import torch
@@ -67,3 +66,56 @@ def test_compare_radial_symmetry_features():
     print(r_mf)
     assert torch.allclose(r_mf, r_ani)
 
+
+def test_compare_angular_symmetry_features(setup_methane):
+
+    import torch
+    from modelforge.potential.utils import AngularSymmetryFunction, triple_by_molecule
+    from openff.units import unit
+    from modelforge.potential.models import _PairList
+
+    species, r, device = setup_methane
+    pairlist = _PairList(only_unique_pairs=True)
+    pairs = pairlist(r[0], torch.tensor([0, 0, 0, 0, 0]))
+    d_ij = pairs["d_ij"].squeeze(1)
+    r_ij = pairs["r_ij"].squeeze(1)
+
+    # reformat for input
+    species = species.flatten()
+    atom_index12 = pairs["pair_indices"]
+    species12 = species[atom_index12]
+    # ANI constants
+    angular_cutoff = Rca = 3.5  # angular_cutoff
+    angular_start = 0.9
+    angular_dist_divisions = 4
+    radial_cutoff = 5.1  # radial_cutoff
+    radial_start = 0.8
+    radial_dist_divisions = 8
+
+    ShfA = torch.linspace(angular_start, angular_cutoff, angular_dist_divisions + 1)[
+        :-1
+    ]
+    ShfR = torch.linspace(radial_start, radial_cutoff, radial_dist_divisions + 1)[:-1]
+
+    asf = AngularSymmetryFunction(
+        angular_dist_divisions,
+        angular_cutoff * unit.angstrom,
+        angular_start * unit.angstrom,
+        radial_dist_divisions,
+        radial_start * unit.angstrom,
+        radial_cutoff * unit.angstrom,
+        ani_style=True,
+    )
+    r_mf = asf(r / 10)
+
+    even_closer_indices = (d_ij <= Rca).nonzero().flatten()
+    atom_index12 = atom_index12.index_select(1, even_closer_indices)
+    species12 = species12.index_select(1, even_closer_indices)
+    r_ij = r_ij.index_select(0, even_closer_indices)
+    central_atom_index, pair_index12, sign12 = triple_by_molecule(atom_index12)
+    species12_small = species12[:, pair_index12]
+    vec12 = vec.index_select(0, pair_index12.view(-1)).view(
+        2, -1, 3
+    ) * sign12.unsqueeze(-1)
+    species12_ = torch.where(sign12 == 1, species12_small[1], species12_small[0])
+    angular_terms_ = angular_terms(Rca, ShfZ, EtaA, Zeta, ShfA, vec12)

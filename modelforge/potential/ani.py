224ea6bb133c95deb4c5735a52fd3d50798740b2
wiederm <marcus.wieder@gmail.com>
2024-02-21 15:45:35 +0000

    Refactor ANIRepresentation initialization and setup radial and angular symmetry functions

--- modelforge/potential/ani.py
+++ modelforge/potential/ani.py
@@ -4,18 +4,59 @@ from loguru import logger as log
 from modelforge.potential.models import BaseNNP
 from modelforge.potential.postprocessing import PostprocessingPipeline, NoPostprocess
 from typing import Dict
+from openff.units import unit
 
 
 class ANIRepresentation(nn.Module):
     # calculate the atomic environment vectors
     # used for the ANI architecture of NNPs
 
-    def __init__(self):
+    def __init__(self, radial_cutoff: unit.Quantity, angular_cutoff: unit.Quantity):
         # radial symmetry functions
-        from .utils import RadialSymmetryFunction, AngularSymmetryFunction
 
-        self.radial_symmetry_functions = RadialSymmetryFunction()
-        self.angular_symmetry_functions = AngularSymmetryFunction()
+        super().__init__()
+        self.radial_cutoff = radial_cutoff
+        self.angular_cutoff = angular_cutoff
+
+        self.radial_symmetry_functions = self._setup_radial_symmetry_functions(
+            self.radial_cutoff
+        )
+        self.angular_symmetry_functions = self._setup_angular_symmetry_functions(
+            self.angular_cutoff
+        )
+
+    def _setup_radial_symmetry_functions(self, radial_cutoff: unit.Quantity):
+        from openff.units import unit
+        from .utils import RadialSymmetryFunction
+
+        # ANI constants
+        radial_start = 0.8 * unit.angstrom
+        radial_dist_divisions = 8
+
+        radial_symmetry_function = RadialSymmetryFunction(
+            radial_dist_divisions,
+            radial_cutoff,
+            radial_start,
+            ani_style=True,
+        )
+        return radial_symmetry_function
+
+    def _setup_angular_symmetry_functions(self, angular_cutoff: unit.Quantity):
+        from .utils import AngularSymmetryFunction
+        from openff.units import unit
+
+        # ANI constants for angular features
+        angular_start = 0.8 * unit.angstrom
+        angular_dist_divisions = 8
+        angle_sections = 4
+
+        # set up modelforge angular features
+        return AngularSymmetryFunction(
+            angular_cutoff,
+            angular_start,
+            angular_dist_divisions,
+            angle_sections,
+        )
 
 
 class ANIInteraction(nn.Module):
@@ -31,6 +72,8 @@ class ANI2x(BaseNNP):
         postprocessing: PostprocessingPipeline = PostprocessingPipeline(
             [NoPostprocess({})]
         ),
+        radial_cutoff: unit.Quantity = 5.3 * unit.angstrom,
+        angular_cutoff: unit.Quantity = 3.5 * unit.angstrom,
     ) -> None:
         """
         Initialize the ANi NNP architeture.
@@ -41,18 +84,19 @@ class ANI2x(BaseNNP):
             Number of filters; defines the dimensionality of the intermediate features (default is 2).
         """
 
-        log.debug("Initializing SchNet model.")
+        log.debug("Initializing ANI model.")
         super().__init__(
-            cutoff=float(cutoff_module.radial_cutoff), postprocessing=postprocessing
+            radial_cutoff=radial_cutoff,
+            angular_cutoff=angular_cutoff,
+            postprocessing=postprocessing,
         )
 
-        # initialize the energy readout
-        from .utils import EnergyReadout
-
         # Initialize representation block
-        self.ani_representation_module = ANIRepresentation(self.radial_basis_module)
+        self.ani_representation_module = ANIRepresentation(
+            radial_cutoff, angular_cutoff
+        )
         # Intialize interaction blocks
-        self.interaction_modules = ANIRepresentation()
+        self.interaction_modules = ANIInteraction()
 
     def _readout(self, inputs: Dict[str, torch.Tensor]) -> torch.Tensor:
         # Compute the energy for each system
@@ -65,12 +109,23 @@ class ANI2x(BaseNNP):
 
     def _model_specific_input_preparation(self, inputs: Dict[str, torch.Tensor]):
         # Perform atomic embedding
-        from modelforge.potential.utils import embed_atom_features
+        # reformat for input
+        species = species.flatten()
+        atom_index12 = inputs["pair_indices"]
+        species12 = species[atom_index12]
 
-        atomic_embedding = embed_atom_features(
-            inputs["atomic_numbers"], self.embedding_module
-        )
-        inputs["atomic_embedding"] = atomic_embedding
+        # get index in right order
+        even_closer_indices = (d_ij <= Rca).nonzero().flatten()
+        atom_index12 = atom_index12.index_select(1, even_closer_indices)
+        species12 = species12.index_select(1, even_closer_indices)
+        r_ij = r_ij.index_select(0, even_closer_indices)
+        central_atom_index, pair_index12, sign12 = triple_by_molecule(atom_index12)
+        species12_small = species12[:, pair_index12]
+        vec12 = r_ij.index_select(0, pair_index12.view(-1)).view(
+            2, -1, 3
+        ) * sign12.unsqueeze(-1)
+        species12_ = torch.where(sign12 == 1, species12_small[1], species12_small[0])
+
         return inputs
 
     def _forward(self, inputs: Dict[str, torch.Tensor]) -> torch.Tensor:

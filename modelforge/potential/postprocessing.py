Fix self-energies calculation and update test assertions

modelforge/potential/postprocessing.py
@@ -96,7 +96,7 @@ class AddSelfEnergies(Postprocess):
         molecule_indices = postprocessing_data["atomic_subsystem_indices"]
         max_atomic_number = torch.max(atomic_numbers).item()
         self_energies_tensor = torch.zeros(max_atomic_number + 1)
-        for atomic_number, energy in self.dataset_statistics["self_energies"].items():
+        for atomic_number, energy in self.dataset_statistics["self_energies"]:
             self_energies_tensor[atomic_number] = energy
 
         # Calculate self energies for each atom and then aggregate them per molecule

modelforge/tests/test_dataset.py
@@ -331,16 +331,16 @@ def test_self_energy():
 
     assert dataset.dataset_statistics
     self_energies = dataset.dataset_statistics["self_energies"]
-    # only 4 elements present in the reduced QM9 dataset
-    assert len(self_energies) == 4
+    # 5 elements present in the QM9 dataset
+    assert len(self_energies) == 5
     # H: -1313.4668615546
-    assert np.isclose(self_energies[1], -1584.5087457646348)
+    assert np.isclose(self_energies[1], -1313.4668615546)
     # C: -99366.70745535441
-    assert np.isclose(self_energies[6], -99960.88941782094)
+    assert np.isclose(self_energies[6], -99366.70745535441)
     # N: -143309.9379722722
-    assert np.isclose(self_energies[7], -143754.0263865598)
+    assert np.isclose(self_energies[7], -143309.9379722722)
     # O: -197082.0671774158
-    assert np.isclose(self_energies[8], -197495.00132926644)
+    assert np.isclose(self_energies[8], -197082.0671774158)
 
 
 

modelforge/tests/test_models.py
@@ -446,8 +446,8 @@ def test_postprocessing():
     # self energy is calculated and removed in prepare_data if `remove_self_energies` is True
     dataset.prepare_data(remove_self_energies=True, normalize=False)
     assert dataset.dataset_statistics
-    # only 4 elements present in the reduced QM9 dataset
-    assert len(dataset.dataset_statistics["self_energies"]) == 4
+    # 5 elements present in the QM9 dataset
+    assert len(dataset.dataset_statistics["self_energies"]) == 5
 
     from modelforge.potential.schnet import SchNET
     from modelforge.potential import CosineCutoff, GaussianRBF
@@ -479,7 +479,6 @@ def test_postprocessing():
     )
 
     for batch in dataset.train_dataloader():
-        print(batch)
         result = model(batch)
         break
 
@@ -518,9 +517,11 @@ def test_postprocessing():
         result_with_offset = model(batch)
         break
 
+    print(f"{offset=}")
+    print(f"{e=}")
     r1 = result_with_offset[0].item()
     r2 = (e + offset).item()
 
-    # make sure that the prediction between the two 
+    # make sure that the prediction between the two
     # SchNET models differ only by the offset
     assert np.isclose(r1, r2)
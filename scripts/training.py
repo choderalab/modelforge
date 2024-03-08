d3adf33ac4f3a5138e0c48023a0e92a82708b65a
wiederm <marcus.wieder@gmail.com>
2024-03-08 13:33:14 +0100

    clean up training script

--- scripts/training.py
+++ scripts/training.py
@@ -1,37 +1,28 @@
 # This is an example script that trains the PaiNN model on the .
 from lightning import Trainer
 import torch
-from modelforge.potential.schnet import LightningSchNET
-
-from modelforge.potential import CosineCutoff, RadialSymmetryFunction
-from modelforge.potential.utils import Embedding
-
-from openff.units import unit
-
-max_atomic_number = 100
-nr_atom_basis = 64
-number_of_gaussians = 14
-nr_interaction_blocks = 2
-
-cutoff = unit.Quantity(5, unit.angstrom)
-embedding = Embedding(max_atomic_number, nr_atom_basis)
-assert embedding.embedding_dim == nr_atom_basis
-radial_symmetry_function_module = RadialSymmetryFunction(
-    number_of_gaussians=number_of_gaussians, radial_cutoff=cutoff
-)
-
-cutoff = CosineCutoff(cutoff=cutoff)
+from modelforge.potential.schnet import SchNET
+from modelforge.potential.ani import ANI2x
 
 from modelforge.dataset.qm9 import QM9Dataset
 from modelforge.dataset.dataset import TorchDataModule
 from modelforge.dataset.utils import FirstComeFirstServeSplittingStrategy
 
+# Set up dataset
 data = QM9Dataset(for_unit_testing=False)
 dataset = TorchDataModule(
     data, batch_size=128, split=FirstComeFirstServeSplittingStrategy()
 )
 
 dataset.prepare_data(remove_self_energies=True, normalize=True)
+
+# Set up model
+model = ANI2x()
+model = model.to(torch.float32)
+
+print(model)
+
+# set up traininer
 
 from lightning.pytorch.callbacks.early_stopping import EarlyStopping
 
@@ -46,20 +37,10 @@ trainer = Trainer(
 )
 
 
-model = LightningSchNET(
-    embedding=embedding,
-    nr_interaction_blocks=nr_interaction_blocks,
-    radial_symmetry_function_module=radial_symmetry_function_module,
-    cutoff_module=cutoff,
-    nr_filters=32,
-    lr=1e-6,
-)
-
-print(model)
 # set scaling and ase values
+
+dataset.dataset_statistics["scaling_stddev"] = 1
 model.dataset_statistics = dataset.dataset_statistics
 
-# Move model to the appropriate dtype and device
-model = model.to(torch.float32)
 # Run training loop and validate
 trainer.fit(model, dataset.train_dataloader(), dataset.val_dataloader())

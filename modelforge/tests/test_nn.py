3e53db240e154ce293e18ef32ca7e55b755b5668
wiederm <marcus.wieder@gmail.com>
2024-03-08 07:39:59 +0100

    remove r_ij rbf test

--- modelforge/tests/test_nn.py
+++ modelforge/tests/test_nn.py
@@ -21,28 +21,3 @@ def test_gaussian_rbf_1D():
         number_of_gaussians=6, radial_cutoff=5.0 * unit.angstrom
     )
 
-
-def test_gaussian_rbf_3D():
-    from openff.units import unit
-
-    dist = torch.tensor([[[0.0, 1.0, 1.5], [0.5, 1.5, 3.0]]])
-    # smear using 4 Gaussian functions with 1. spacing
-    smear = RadialSymmetryFunction(
-        radial_start=0.1 * unit.angstrom,
-        radial_cutoff=4.0 * unit.angstrom,
-        number_of_gaussians=4,
-    )
-    # absolute value of centered distances
-    expt = torch.tensor(
-        [
-            [
-                [[1, 2, 3, 4], [0, 1, 2, 3], [0.5, 0.5, 1.5, 2.5]],
-                [[0.5, 1.5, 2.5, 3.5], [0.5, 0.5, 1.5, 2.5], [2, 1, 0, 1]],
-            ]
-        ]
-    )
-    expt = torch.exp(-1 * expt**2)
-    print(expt)
-    print(smear(dist))
-    assert torch.allclose(expt, smear(dist), atol=1e-5)
-    assert list(smear.parameters()) == []

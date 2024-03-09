601f0e5d0ed4841317bcfeb3c63283c966417d20
wiederm <marcus.wieder@gmail.com>
2024-03-09 21:22:02 +0100

    bugfix

--- modelforge/potential/models.py
+++ modelforge/potential/models.py
@@ -193,7 +193,7 @@ class BaseNNP(pl.LightningModule):
         self._log_message_dtype = False
         self._log_message_units = False
         self._dataset_statistics: Dict = {
-            "scaling_mean": 1.0,
+            "scaling_mean": 0.0,
             "scaling_stddev": 1.0,
             "atomic_self_energies": AtomicSelfEnergies(),
         }

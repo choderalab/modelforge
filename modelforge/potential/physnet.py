7628071aa8285c85f0342c64d5de8aeb4dacc57e
wiederm <marcus.wieder@gmail.com>
2024-03-11 16:43:34 +0100

    rouchg outline of physnet

--- modelforge/potential/physnet.py
+++ modelforge/potential/physnet.py
@@ -1,16 +1,125 @@
-from .models import BaseNNP
+from .models import BaseNeuralNetworkPotential
 from loguru import logger as log
 from openff.units import unit
 import torch
 from typing import Dict
+from torch import nn
+
+class PhysNetRepresentation(nn.Module):
+
+    def __init__(
+        self,
+        cutoff: unit = 5 * unit.angstrom,
+        number_of_gaussians: int = 16,
+        device:torch.device = torch.device('cpu')
+    ):
+        super().__init__()
+
+        # cutoff
+        from modelforge.potential import CosineCutoff
+
+        self.cutoff_module = CosineCutoff(cutoff, device)
+
+        # radial symmetry function
+        from .utils import RadialSymmetryFunction
+
+        self.radial_symmetry_function_module = RadialSymmetryFunction(
+            number_of_gaussians=number_of_gaussians,
+            radial_cutoff=cutoff,
+            ani_style=False,
+            dtype=torch.float32,
+        )
+
+    def forward(self, inputs: Dict[str, torch.Tensor]):
+        """
+        Generates the representation for the Physnet potential.
+
+        Parameters
+        ----------
+        inputs (Dict[str, torch.Tensor]): A dictionary containing the input tensors.
+            - "d_ij":  torch.Tensor, shape: (n_pairs, 1, distance)
+            Pairwise distances between atoms.
+
+        Returns:
+        ----------
+        f_ij: torch.Tensor:
+            radial basis function expension, shape (n_pairs, 1, n_gaussians)
+        """
+
+        rbf = self.radial_symmetry_function_module(inputs["d_ij"])
+        cutoff = self.cutoff_module(inputs["d_ij"])
+        f_ij = torch.mul(rbf, cutoff)
+        return f_ij
 
 
-class PhysNET(BaseNNP):
+class PhysNetInteraction(nn.Module):
+
+    def __init__():
+        pass
+
+
+    def forward(self, inputs: Dict[str, torch.Tensor])
+    
+        """
+        PhysNetInteraction module. Takes as input the embedded nuclear charges 
+        and the expended radial basis functions (already with cutoff added).
+
+        Parameters:
+        ----------
+        inputs (Dict[str, torch.Tensor]): A dictionary containing the input tensors.
+            - "f_ij": torch.Tensor, shape: (n_pairs, 1, n_gaussians)
+                radial basis function expension, shape (n_pairs, 1, n_gaussians)
+            - "atomic_embedding": torch.Tensor, shape: (nr_of_atoms_in_systems, 1, nr_atom_basis)
+                Embeddings of atomic numbers. Shape: (n_atoms, embedding_dim).
+            - "pairlist: torch.Tensor, shape: (2, nr_of_pairs)
+                Pairlist of the systems.
+
+        Returns:
+            _type_: _description_
+        """
+        from .utils import ShiftedSoftplus
+        softplus = ShiftedSoftplus()
+        
+        atom_embedding = inputs["atomic_embedding"]
+        # Start with embedded features
+        x = 
+
+class PhysNetResidual(nn.Module):
+    def __init__():
+        super().__init__()
+
+    def forward():
+        pass
+
+class PhysNetOutput(nn.Module):
+    def __init__():
+        super().__init__()
+        
+    def forward():
+        pass
+
+class PhysNetModule(nn.Module):
+    
+    def __init__(self):
+        super().__init__()
+        
+        # this class combines the PhysNetInteraction, PhysNetResidual and 
+        # PhysNetOutput class
+        
+        self.interaction = PhysNetInteraction()
+        self.residula = PhysNetResidual()
+        self.output = PhysNetOutput()
+        
+    def forward():
+        pass
+
+class PhysNet(BaseNeuralNetworkPotential):
     def __init__(
         self,
         max_Z: int = 100,
         embedding_dimensions: int = 64,
         cutoff: unit.Quantity = 5 * unit.angstrom,
+        nr_of_modules : int = 2
     ) -> None:
         """
         Initialize the PhysNET class.
@@ -42,13 +151,20 @@ class PhysNET(BaseNNP):
 
         # cutoff
         from modelforge.potential import CosineCutoff
-
         self.cutoff_module = CosineCutoff(cutoff, self.device)
 
         # initialize the energy readout
         from .utils import EnergyReadout
 
         self.readout_module = EnergyReadout(embedding_dimensions)
+
+        self.physnet_representation_module = PhysNetRepresentation(
+            radial_cutoff=cutoff, number_of_gaussians=16
+        )
+
+        # initialize the PhysNetModule building blocks
+        from torch.nn import ModuleList
+        self.physnet_module = PhysNetModule([PhysNetModule() for module_idx in range(nr_of_modules)])
 
     def _readout(self, inputs: Dict[str, torch.Tensor]) -> torch.Tensor:
         # Compute the energy for each system
@@ -67,14 +183,19 @@ class PhysNET(BaseNNP):
         inputs : Dict[str, torch.Tensor]
         - atomic_embedding : torch.Tensor
             Atomic numbers embedding; shape (nr_of_atoms_in_systems, 1, nr_atom_basis).
-        - pairlist:  shape (n_pairs, 2)
-        - r_ij:  shape (n_pairs, 3)
+        - pairlist:  shape (2, n_pairs)
         - d_ij:  shape (n_pairs, 1)
-        - positions:  shape (nr_of_atoms_per_molecules, 3)
         - atomic_embedding:  shape (nr_of_atoms_in_systems, nr_atom_basis)
         Returns
         -------
         torch.Tensor
             Calculated energies; shape (nr_systems,).
         """
-        pass
+        
+        # calculate rbf
+        representation = self.physnet_representation_module(inputs["d_ij"])
+        
+        
+        output = self.physnet_module(inputs, representation)
+
+        self._readout(output)

40dfa8774ca2b305ab4c1a6b7fac49fc6a37d268
wiederm <marcus.wieder@gmail.com>
2024-03-12 12:28:45 +0100

    PhysNet Interaction Module

--- modelforge/potential/physnet.py
+++ modelforge/potential/physnet.py
@@ -14,6 +14,20 @@ class PhysNetRepresentation(nn.Module):
         number_of_gaussians: int = 16,
         device: torch.device = torch.device("cpu"),
     ):
+        """
+        Representation module for the PhysNet potential, handling the generation of
+        the radial basis functions (RBFs) with a cutoff.
+
+        Parameters
+        ----------
+        cutoff : openff.units.unit.Quantity, default=5*unit.angstrom
+            The cutoff distance for interactions.
+        number_of_gaussians : int, default=16
+            Number of Gaussian functions to use in the radial basis function.
+        device : torch.device, default=torch.device("cpu")
+            The device on which to perform the computations.
+        """
+
         super().__init__()
 
         # cutoff
@@ -31,81 +45,153 @@ class PhysNetRepresentation(nn.Module):
             dtype=torch.float32,
         )
 
-    def forward(self, inputs: Dict[str, torch.Tensor]):
+    def forward(self, d_ij: torch.Tensor) -> torch.Tensor:
         """
-        Generates the representation for the Physnet potential.
+        Forward pass of the representation module.
 
         Parameters
         ----------
-        inputs (Dict[str, torch.Tensor]): A dictionary containing the input tensors.
-            - "d_ij":  torch.Tensor, shape: (n_pairs, 1, distance)
-            Pairwise distances between atoms.
+        d_ij : torch.Tensor
+            pairwise distances between atoms, shape (n_pairs, 1).
 
-        Returns:
-        ----------
-        f_ij: torch.Tensor:
-            radial basis function expension, shape (n_pairs, 1, n_gaussians)
+        Returns
+        -------
+        torch.Tensor
+            The radial basis function expansion applied to the input distances,
+            shape (n_pairs, 1, n_gaussians), after applying the cutoff function.
         """
 
-        rbf = self.radial_symmetry_function_module(inputs["d_ij"])
-        cutoff = self.cutoff_module(inputs["d_ij"])
+        rbf = self.radial_symmetry_function_module(d_ij)
+        cutoff = self.cutoff_module(d_ij)
         f_ij = torch.mul(rbf, cutoff)
         return f_ij
 
 
+class GaussianLogarithmAttention(nn.Module):
+    def __init__(self, input_features: int = 1, output_features: int = 64):
+        super().__init__()
+        # This layer approximates the Gaussian Logarithm function for attention
+        self.linear = nn.Linear(input_features, output_features)
+        self.activation = nn.Softplus()
+
+    def forward(self, distances: torch.Tensor) -> torch.Tensor:
+        # Apply the GloG function
+        # distances shape: (nr_of_atoms_in_batch, 1)
+
+        logits = self.linear(distances)
+        attention_weights = self.activation(logits)
+        return attention_weights
+
+
 class PhysNetInteraction(nn.Module):
 
-    def __init__(self):
-        pass
-
-    def forward(self, inputs: Dict[str, torch.Tensor]):
+    def __init__(self, number_of_atom_basis: int = 16):
         """
-        PhysNetInteraction module. Takes as input the embedded nuclear charges
-        and the expended radial basis functions (already with cutoff added).
+        Interaction module for PhysNet, which is responsible for processing the
+        embedded nuclear charges and the expanded radial basis functions.
 
-        Parameters:
-        ----------
-        inputs (Dict[str, torch.Tensor]): A dictionary containing the input tensors.
-            - "f_ij": torch.Tensor, shape: (n_pairs, 1, n_gaussians)
-                radial basis function expension, shape (n_pairs, 1, n_gaussians)
-            - "atomic_embedding": torch.Tensor, shape: (nr_of_atoms_in_systems, 1, nr_atom_basis)
-                Embeddings of atomic numbers. Shape: (n_atoms, embedding_dim).
-            - "pairlist: torch.Tensor, shape: (2, nr_of_pairs)
-                Pairlist of the systems.
-
-        Returns:
-            _type_: _description_
+        This module currently acts as a placeholder to be expanded upon.
         """
         from .utils import ShiftedSoftplus
 
-        softplus = ShiftedSoftplus()
+        super().__init__()
+        self.glog_attention = GaussianLogarithmAttention(
+            output_features=number_of_atom_basis
+        )
+        self.activation_function = ShiftedSoftplus()
+        self.interaction_i = nn.Sequential(
+            nn.Linear(number_of_atom_basis, 2 * number_of_atom_basis),
+            nn.Softplus(),
+            nn.Linear(2 * number_of_atom_basis, 1),
+        )
+        self.interaction_j = nn.Sequential(
+            nn.Linear(number_of_atom_basis, 2 * number_of_atom_basis),
+            nn.Softplus(),
+            nn.Linear(2 * number_of_atom_basis, 1),
+        )
 
-        atom_embedding = inputs["atomic_embedding"]
-        # Start with embedded features
-        x = softplus(atom_embedding)
+    def forward(self, inputs: Dict[str, torch.Tensor]):
+        """
+        Forward pass of the interaction module. Currently, a placeholder that
+        needs further implementation.
 
+        Parameters
+        ----------
+        inputs : Dict[str, torch.Tensor]
+            A dictionary containing the input tensors. Expected tensors include
+            "f_ij" for radial basis function expansions, "atomic_embedding" for
+            embeddings of atomic numbers, and "pairlist" for the pair lists.
+
+        Returns
+        -------
+        """
+
+        # extract relevant variables
         idx_i, idx_j = inputs["pairlist"]
+        f_ij = inputs["f_ij"]  # shape: (n_pairs, 1, n_gaussians)
+        d_ij = inputs["d_ij"]  # shape: (n_pairs, 1)
+        atom_embedding = inputs["atomic_embedding"] #shape (nr_of_atoms_in_batch, embedding dim)
+        
+        x_i = x[idx_i]
+        x_j = x[idx_j]
+
+        # Start with embedded features
+        x = self.activation_function(atom_embedding)
+
+        # this function implements equation 6 from the PhysNet manuscript
+    
+        # Equation 6 implementation overview:
+        # v_tilde_i = x_i_prime + sum_over_j(x_j_prime * f_ij_prime)
+        # where:
+        # - x_i_prime and x_j_prime are the features of atoms i and j, respectively, processed through separate networks.
+        # - f_ij_prime represents the modulated radial basis functions (f_ij) by the Gaussian Logarithm Attention weights.
+
+        # Obtain attention mask G(r_ij)
+        # Get attention weights from distances
+        attention_weights = self.glog_attention(d_ij)
+
+        # Draft proto message v_tilde
+        # x_i_prime
+        x_i_prime = self.interaction_i(x_i)
+        # x_j_prime
+        x_j_prime = self.interaction_j(x_j)
+        # f_ij_prime
+        f_ij_prime = torch.mul(f_ij, attention_weights)
+
+        # Compute the interaction for x_j
+        x_j_prime = torch.mul(x_j_prime, f_ij_prime)
+
+        # gating vectors
+        # u =
 
 
 class PhysNetResidual(nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward():
-        pass
+    def forward(inputs: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        return inputs
 
 
 class PhysNetOutput(nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward():
-        pass
+    def forward(inputs: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        return inputs
 
 
 class PhysNetModule(nn.Module):
 
     def __init__(self):
+        """
+        Wrapper module that combines the PhysNetInteraction, PhysNetResidual, and
+        PhysNetOutput classes into a single module. This serves as the building
+        block for the PhysNet model.
+
+        This is a skeletal implementation that needs to be expanded upon.
+        """
+
         super().__init__()
 
         # this class combines the PhysNetInteraction, PhysNetResidual and
@@ -115,8 +201,13 @@ class PhysNetModule(nn.Module):
         self.residula = PhysNetResidual()
         self.output = PhysNetOutput()
 
-    def forward():
-        pass
+    def forward(self, inputs: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        """
+        Forward pass for the PhysNet module. Currently, a placeholder that
+        needs further implementation.
+        """
+        # Placeholder for actual module operations.
+        return inputs
 
 
 class PhysNet(BaseNeuralNetworkPotential):
@@ -128,20 +219,18 @@ class PhysNet(BaseNeuralNetworkPotential):
         nr_of_modules: int = 2,
     ) -> None:
         """
-        Initialize the PhysNET class.
-        Physnet is a neural network potential first described here: https://arxiv.org/pdf/1902.08408.pdf
-        The potential takes as input the nuclear charges (Z_i) and pairwise distances (r_i)
-        and outputs the atomic energies E_i and partial charges q_i.
-        The architecture has the following components:
-        - embedding layer: transforming nuclear charges into a vector representation
-        - radial basis function layer: transforming the pairwise distances into a vector representation
-        - module layer (see Fig 2 for graphical representation):
-            embedding and radial basis output are provided as input,
-            consists of (1) interaction, (2) residula and (3) output module
+        Implementation of the PhysNet neural network potential.
+
         Parameters
         ----------
-        max_Z : nn.int
-        embedding_dimensions : int
+        max_Z : int, default=100
+            Maximum atomic number to be embedded.
+        embedding_dimensions : int, default=64
+            Dimension of the embedding vectors for atomic numbers.
+        cutoff : openff.units.unit.Quantity, default=5*unit.angstrom
+            The cutoff distance for interactions.
+        nr_of_modules : int, default=2
+            Number of PhysNet modules to be stacked in the network.
         """
 
         log.debug("Initializing PhysNET model.")
@@ -161,9 +250,9 @@ class PhysNet(BaseNeuralNetworkPotential):
         self.cutoff_module = CosineCutoff(cutoff, self.device)
 
         # initialize the energy readout
-        # from .utils import EnergyReadout
+        from .utils import FromAtomToMoleculeReduction
 
-        # self.readout_module = EnergyReadout(embedding_dimensions)
+        self.readout_module = FromAtomToMoleculeReduction(embedding_dimensions)
 
         self.physnet_representation_module = PhysNetRepresentation(
             cutoff=cutoff, number_of_gaussians=16
@@ -178,7 +267,8 @@ class PhysNet(BaseNeuralNetworkPotential):
 
     def _readout(self, inputs: Dict[str, torch.Tensor]) -> torch.Tensor:
         # Compute the energy for each system
-        return self.readout_module(inputs)
+        # return self.readout_module(inputs)
+        return inputs
 
     def _model_specific_input_preparation(self, inputs: Dict[str, torch.Tensor]):
         # Perform atomic embedding
@@ -202,9 +292,13 @@ class PhysNet(BaseNeuralNetworkPotential):
             Calculated energies; shape (nr_systems,).
         """
 
-        # calculate rbf
+        # Computed representation
         representation = self.physnet_representation_module(inputs["d_ij"])
+        inputs["representation"] = representation
 
-        output = self.physnet_module(inputs, representation)
+        for module in self.physnet_module:
+            output = module(inputs)
 
-        self._readout(output)
+        return torch.rand_like(inputs["d_ij"])
+
+        # return self._readout(output)

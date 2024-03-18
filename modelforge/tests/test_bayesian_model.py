9f4e891d7eea361dd316762a9c87747db72eb24c
wiederm <marcus.wieder@gmail.com>
2024-03-18 22:34:07 +0100

    Refactor test_bayesian_model.py: Simplify SchNet initialization

--- modelforge/tests/test_bayesian_model.py
+++ modelforge/tests/test_bayesian_model.py
@@ -1,21 +1,12 @@
 import pyro
-from modelforge.potential import CosineCutoff, GaussianRBF
-from modelforge.potential.utils import SlicedEmbedding
-from modelforge.potential.schnet import SchNET
+from modelforge.potential import SchNet
 from modelforge.potential.bayesian_models import BayesianAutoNormalPotential
 from .helper_functions import SIMPLIFIED_INPUT_DATA
 
+
 def test_bayesian_model():
     # initialize a vanilla SchNet model
-    embedding = SlicedEmbedding(8, 16, sliced_dim=0)
-    rbf = GaussianRBF(n_rbf=8, cutoff=5.0)
-    cutoff = CosineCutoff(5.0)
-    schnet = SchNET(
-        embedding=embedding,
-        cutoff=cutoff,
-        nr_interaction_blocks=8,
-        radial_basis=rbf,
-    )
+    schnet = SchNet()
 
     # make a Bayesian model from the SchNet
     schnet = BayesianAutoNormalPotential(schnet, log_sigma=1e-2)
@@ -32,7 +23,3 @@ def test_bayesian_model():
 
     # calculate VI loss
     svi.step(SIMPLIFIED_INPUT_DATA, y=0.0)
-
-
-
-

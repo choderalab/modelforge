6da5f3da6de9c48352a610d10d9a6ad0c41eb710
wiederm <marcus.wieder@gmail.com>
2024-05-05 23:14:54 +0200

    feat: Add hyperparameter prior distribution for SchNet model configuration

--- modelforge/potential/physnet.py
+++ modelforge/potential/physnet.py
@@ -602,3 +602,19 @@ class PhysNet(BaseNetwork):
         self.input_preparation = InputPreparation(
             cutoff=cutoff, only_unique_pairs=self.only_unique_pairs
         )
+
+    def _config_prior(self):
+        log.info("Configuring SchNet model hyperparameter prior distribution")
+        from ray import tune
+
+        from modelforge.potential.utils import shared_config_prior
+
+        prior = {
+            "number_of_atom_features": tune.randint(2, 256),
+            "number_of_modules": tune.randint(2, 8),
+            "number_of_interaction_residual": tune.randint(2, 5),
+            "cutoff": tune.uniform(5, 10),
+            "number_of_radial_basis_functions": tune.randint(8, 32),
+        }
+        prior.update(shared_config_prior())
+        return prior

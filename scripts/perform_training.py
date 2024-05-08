cdf9f67b30341c1be9f8af163823e2de947794ed
wiederm <marcus.wieder@gmail.com>
2024-05-08 23:07:41 +0200

    update trianing script

--- scripts/perform_training.py
+++ scripts/perform_training.py
@@ -5,49 +5,64 @@ from modelforge.dataset.dataset import DataModule
 from modelforge.dataset.utils import RandomRecordSplittingStrategy
 from pytorch_lightning.loggers import TensorBoardLogger
 
-# set up tensor board logger
-logger = TensorBoardLogger("tb_logs", name="training")
-model_name = "PhysNet"
-dataset_name = "QM9"
 
-# Set up dataset
-dm = DataModule(
-    name=dataset_name,
-    batch_size=512,
-    splitting_strategy=RandomRecordSplittingStrategy(),
-    normalize=False,
-    for_unit_testing=True,
-)
-# Set up model
-model = NeuralNetworkPotentialFactory.create_nnp("training", model_name)
+def perform_training(
+    model_name: str, dataset_name: str, nr_of_epochs: int, accelerator: str = "cpu"
+):
+    # set up tensor board logger
+    logger = TensorBoardLogger("tb_logs", name="training")
 
-# set up traininer
-from lightning.pytorch.callbacks.early_stopping import EarlyStopping
+    # Set up dataset
+    dm = DataModule(
+        name=dataset_name,
+        batch_size=512,
+        splitting_strategy=RandomRecordSplittingStrategy(),
+        remove_self_energies=True,
+        for_unit_testing=True,
+    )
+    # Set up model
+    model = NeuralNetworkPotentialFactory.create_nnp("training", model_name)
 
-trainer = Trainer(
-    max_epochs=1000,
-    num_nodes=1,
-    devices=1,
-    accelerator="cpu",
-    logger=logger,  # Add the logger here
-    callbacks=[
-        EarlyStopping(
-            monitor="epoch_rmse_val_loss", min_delta=0.05, patience=20, verbose=True
-        )
-    ],
-)
+    # set up traininer
+    from lightning.pytorch.callbacks.early_stopping import EarlyStopping
 
+    trainer = Trainer(
+        max_epochs=nr_of_epochs,
+        num_nodes=1,
+        devices=1,
+        accelerator=accelerator,
+        logger=logger,  # Add the logger here
+        callbacks=[
+            EarlyStopping(
+                monitor="epoch_rmse_val_loss", min_delta=0.05, patience=20, verbose=True
+            )
+        ],
+    )
 
-dm.prepare_data()
-dm.setup()
+    dm.prepare_data()
+    dm.setup()
 
-from modelforge.utils.misc import visualize_model
+    from modelforge.utils.misc import visualize_model
 
-visualize_model(dm, model_name)
+    visualize_model(dm, model_name)
 
-# Run training loop and validate
-trainer.fit(
-    model, train_dataloaders=dm.train_dataloader(), val_dataloaders=dm.val_dataloader()
-)
+    # Run training loop and validate
+    trainer.fit(
+        model,
+        train_dataloaders=dm.train_dataloader(),
+        val_dataloaders=dm.val_dataloader(),
+    )
+
 
 # tensorboard --logdir tb_logs
+
+
+if __name__ == "__main__":
+
+    model_name = "PhysNet"
+    dataset_name = "QM9"
+    nr_of_repeats = 5
+    # Run training loop and validate
+    for i in range(nr_of_repeats):
+        print("Running training iteration:", i)
+        perform_training(model_name, dataset_name, nr_of_epochs=1000, accelerator="gpu")

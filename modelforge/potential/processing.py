90ce4ff4f941a606e993ad98b7ee60dd110baa80
wiederm <marcus.wieder@gmail.com>
2024-06-04 15:47:47 +0200

    chore: Register 'E_i_mean' and 'E_i_stddev' as torch buffers in FromAtomToMoleculeReduction

--- modelforge/potential/processing.py
+++ modelforge/potential/processing.py
@@ -13,8 +13,9 @@ class FromAtomToMoleculeReduction(torch.nn.Module):
         Performs the reduction of 'per_atom' property to 'per_molecule' property.
         """
         super().__init__()
-        self.E_i_mean = torch.tensor([0.0])
-        self.E_i_stddev = torch.tensor([1.0])
+        # turn the following parameters in torch buffer
+        self.register_buffer("E_i_mean", torch.tensor([0.0]))
+        self.register_buffer("E_i_stddev", torch.tensor([1.0]))
 
     def forward(
         self, x: torch.Tensor, atomic_subsystem_indices: torch.Tensor

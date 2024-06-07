ea1da3dd650a70c507e538a9335950f1b7c84490
wiederm <marcus.wieder@gmail.com>
2024-06-07 18:20:27 +0200

    Refactor potential/processing.py for improved parameter handling

--- modelforge/potential/processing.py
+++ modelforge/potential/processing.py
@@ -14,8 +14,8 @@ class FromAtomToMoleculeReduction(torch.nn.Module):
         """
         super().__init__()
         # turn the following parameters in torch buffer
-        self.register_buffer("E_i_mean", torch.tensor([0.0]))
-        self.register_buffer("E_i_stddev", torch.tensor([1.0]))
+        self.E_i_mean = torch.tensor([0.0])
+        self.E_i_stddev = torch.tensor([1.0])
 
     def forward(
         self, x: torch.Tensor, atomic_subsystem_indices: torch.Tensor

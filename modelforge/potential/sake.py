37b6ed89a64f5bcca76511ecfd4b7ed1c24071f0
Arnav Nagle <rnow03@gmail.com>
2024-07-09 11:02:19 -0700

    Remove unused d_ij parameter

--- modelforge/potential/sake.py
+++ modelforge/potential/sake.py
@@ -458,7 +458,7 @@ class SAKEInteraction(nn.Module):
         )
         return zeros.scatter_add(0, expanded_idx_i, h_ij_semantic)
 
-    def get_semantic_attention(self, h_ij_edge, idx_i, idx_j, d_ij, nr_atoms):
+    def get_semantic_attention(self, h_ij_edge, idx_i, idx_j, nr_atoms):
         """Compute semantic attention. Softmax is over all senders connected to a receiver.
 
         Wang and Chodera (2023) Sec. 5 Eq. 9-10.
@@ -471,8 +471,6 @@ class SAKEInteraction(nn.Module):
             Indices of the receiver nodes. Shape [nr_pairs, ].
         idx_j : torch.Tensor
             Indices of the sender nodes. Shape [nr_pairs, ].
-        d_ij : torch.Tensor
-            Distance between senders and receivers. Shape [nr_pairs, ].
         nr_atoms : int
             Number of atoms in all systems.
 
@@ -526,7 +524,7 @@ class SAKEInteraction(nn.Module):
 
         h_ij_edge = self.update_edge(h[idx_j], h[idx_i], d_ij)
         h_ij_semantic = self.get_semantic_attention(
-            h_ij_edge, idx_i, idx_j, d_ij, nr_of_atoms_in_all_systems
+            h_ij_edge, idx_i, idx_j, nr_of_atoms_in_all_systems
         )
         del h_ij_edge
         h_i_semantic = self.aggregate(h_ij_semantic, idx_i, nr_of_atoms_in_all_systems)

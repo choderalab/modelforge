f7234ffd7d4515e784974498291d256a8776a041
Ian Kenney <ianmichaelkenney@gmail.com>
2024-07-10 10:01:45 -0700

    Preparing tests for AIMNet2
    * XFAIL on current known failures for tests or
      those that are not implemented
    * Moved imports to start of file

--- modelforge/tests/test_aimnet2.py
+++ modelforge/tests/test_aimnet2.py
@@ -1,37 +1,40 @@
+import pytest
+import torch
+
+from modelforge.potential.aimnet2 import AimNet2
+from modelforge.tests.test_models import load_configs
+
+
 def test_initialize_model():
     """Test initialization of the Schnet model."""
-    from modelforge.potential.aimnet2 import AimNet2
-
-    from modelforge.tests.test_models import load_configs
 
     # read default parameters
     config = load_configs(f"schnet_without_ase", "qm9")
+
     # Extract parameters
     potential_parameter = config["potential"].get("potential_parameter", {})
     aimnet = AimNet2(**potential_parameter)
     assert aimnet is not None, "Aimnet2 model should be initialized."
 
 
+@pytest.mark.xfail(raises=NotImplementedError)
 def test_rbf():
-    pass
+    raise NotImplementedError
 
 
+@pytest.mark.xfail(raises=AttributeError)
 def test_forward(single_batch_with_batchsize_64):
     """Test initialization of the Schnet model."""
-    from modelforge.potential.aimnet2 import AimNet2
-
-    from modelforge.tests.test_models import load_configs
-
     # read default parameters
     config = load_configs(f"schnet_without_ase", "qm9")
     # Extract parameters
     potential_parameter = config["potential"].get("potential_parameter", {})
     aimnet = AimNet2(**potential_parameter)
     assert aimnet is not None, "Aimnet model should be initialized."
-    import torch
 
     y_hat = aimnet(single_batch_with_batchsize_64.nnp_input.to(dtype=torch.float32))
 
 
+@pytest.mark.xfail(raises=NotImplementedError)
 def test_against_original_implementation():
-    pass
+    raise NotImplementedError

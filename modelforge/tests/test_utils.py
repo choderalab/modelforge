a0e6a379f360ef0adb3a0bfe3c7cab9cdfade243
Arnav Nagle <43835955+ArnNag@users.noreply.github.com>
2024-07-15 23:20:36 -0700

    Refactor radial basis functions (#171)
    * Begin refactoring radial basis functions
    
    * Update RadialBasisFunction comment
    
    * Fix references to radial basis centers and scale factors
    
    * Continue refactoring radial basis functions
    
    * Fix SchNet RBF
    
    * Fix PhysNet and ANI RBF
    
    * Trying to fix SAKE but failing
    
    * Fix SAKE test
    
    * Remove unnecessary test
    
    * Refactor prefactor
    
    * Update comment for nondimensionalization
    
    * Fix SAKE and ANI bugs
    
    * Fix test radial symmetry function
    
    * Update utils.py
    
    Small changes to the dtype that is passed to the RBF. I think it is best to set a default value for the publically exposed class, e.g., `SchNetRadialBasisFunction`) and have optional `dtype` parameters present in the internal classes (which will then fail if it wasn't set in the entry point classes).
    
    Also added parameter docstring to some of the RBF classes.
    
    * typo fix
    
    * Make RadialBasisFunctionCore inherit from nn.Module
    
    * Remove @staticmethod decorator
    
    * Add shape assertion in RadialBasisFunctionCore. Remove unnecessary unsqueeze in PhysNet radial basis function
    
    * Remove print statement
    
    * Remove print statements in SAKE
    
    * Fix SAKE RBF test
    
    * Fix SchNet tests
    
    * Fix spk tests
    
    * Clean spk test
    
    * Fix PaiNN spk test
    
    * Fix PhysNet test_rbf
    
    * Fix docstring reference
    
    * Add alpha hyperparameter
    
    * Use units within initialization of PhysNet centers and scale factors
    
    * Move dtype casting in SchNet
    
    * Remove print statements
    
    * Fix schnet rbf test
    
    * Remove whitespace changes
    
    * More whitespace changes and remove unreachable code
    
    * Remove print statement
    
    * Remove another whitespace change
    
    * Remove print statements
    
    * Update test_sake.py
    
    * Update ani.py
    
    * Run black
    
    * Run black
    
    ---------
    
    Co-authored-by: Marcus Wieder <31651017+wiederm@users.noreply.github.com>

--- modelforge/tests/test_utils.py
+++ modelforge/tests/test_utils.py
@@ -2,7 +2,6 @@ import numpy as np
 import torch
 import pytest
 
-from modelforge.potential.utils import CosineCutoff, RadialSymmetryFunction
 
 
 @pytest.fixture(scope="session")
@@ -79,6 +78,7 @@ def test_cosine_cutoff():
     """
     Test the cosine cutoff implementation.
     """
+    from modelforge.potential.utils import CosineCutoff
     # Define inputs
     x = torch.Tensor([1, 2, 3])
     y = torch.Tensor([4, 5, 6])
@@ -102,6 +102,7 @@ def test_cosine_cutoff():
 
 def test_cosine_cutoff_module():
     # Test CosineCutoff module
+    from modelforge.potential.utils import CosineCutoff
     from openff.units import unit
 
     # test the cutoff on this distance vector (NOTE: it is in angstrom)
@@ -122,13 +123,46 @@ def test_radial_symmetry_function_implementation():
     """
     Test the Radial Symmetry function implementation.
     """
-    from modelforge.potential.utils import RadialSymmetryFunction, CosineCutoff
     import torch
     from openff.units import unit
     import numpy as np
+    from modelforge.potential.utils import CosineCutoff, GaussianRadialBasisFunctionWithScaling
 
     cutoff_module = CosineCutoff(cutoff=unit.Quantity(5.0, unit.angstrom))
-    RSF = RadialSymmetryFunction(
+
+    class RadialSymmetryFunctionTest(GaussianRadialBasisFunctionWithScaling):
+        @staticmethod
+        def calculate_radial_basis_centers(
+                number_of_radial_basis_functions,
+                _max_distance_in_nanometer,
+                _min_distance_in_nanometer,
+                dtype,
+        ):
+            centers = torch.linspace(
+                _min_distance_in_nanometer,
+                _max_distance_in_nanometer,
+                number_of_radial_basis_functions,
+                dtype=dtype,
+            )
+            return centers
+
+        @staticmethod
+        def calculate_radial_scale_factor(
+                number_of_radial_basis_functions,
+                _max_distance_in_nanometer,
+                _min_distance_in_nanometer,
+                dtype
+        ):
+            scale_factors = torch.full(
+                (number_of_radial_basis_functions,),
+                (_min_distance_in_nanometer - _max_distance_in_nanometer)
+                / number_of_radial_basis_functions,
+            )
+            scale_factors = (scale_factors * -15_000) ** -0.5
+            return scale_factors
+
+
+    RSF = RadialSymmetryFunctionTest(
         number_of_radial_basis_functions=18,
         max_distance=unit.Quantity(5.0, unit.angstrom),
     )

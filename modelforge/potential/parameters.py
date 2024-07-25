a9cb02a221aed7200e9e366c49845b54a9ec9e2a
chrisiacovella <chris.iacovella@gmail.com>
2024-07-24 21:41:04 -0700

    removing defaults from pydantic models, added in automatic unit conversion  for strings to unit quantities

--- modelforge/potential/parameters.py
+++ modelforge/potential/parameters.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
+from openff.units import unit
+from typing import Union
 
 """
 This module contains pydantic models for storing the parameters of 
@@ -12,39 +14,49 @@ class ParametersBase(BaseModel):
         use_enum_values = True
 
 
+# define a reusable validator for converting string to unit.Quantity
+def convert_str_to_unit(value: Union[str, unit.Quantity]) -> unit.Quantity:
+    if isinstance(value, str):
+        return unit.Quantity(value)
+    return value
+
+
 class GeneralPostProcessingOperation(ParametersBase):
-    calculate_molecular_self_energy: bool = False
-    calculate_atomic_self_energy: bool = False
+    calculate_molecular_self_energy: bool
+    calculate_atomic_self_energy: bool
 
 
 class PerAtomEnergy(ParametersBase):
-    normalize: bool = False
-    from_atom_to_molecule_reduction: bool = False
-    keep_per_atom_property: bool = False
+    normalize: bool
+    from_atom_to_molecule_reduction: bool
+    keep_per_atom_property: bool
 
 
 class ANI2xParameters(ParametersBase):
     class CoreParameter(ParametersBase):
-        angle_sections: int = 4
-        radial_max_distance: str = "5.1 angstrom"
-        radial_min_distance: str = "0.8 angstrom"
-        number_of_radial_basis_functions: int = 16
-        angular_max_distance: str = "3.5 angstrom"
-        angular_min_distance: str = "0.8 angstrom"
-        angular_dist_divisions: int = 8
+        angle_sections: int
+        radial_max_distance: Union[str, unit.Quantity]
+        radial_min_distance: Union[str, unit.Quantity]
+        number_of_radial_basis_functions: int
+        angular_max_distance: Union[str, unit.Quantity]
+        angular_min_distance: Union[str, unit.Quantity]
+        angular_dist_divisions: int
+
+        converted_units = field_validator(
+            "radial_max_distance",
+            "radial_min_distance",
+            "angular_max_distance",
+            "angular_min_distance",
+        )(convert_str_to_unit)
+        # def convert_to_unit(cls, value) -> unit.Quantity:
+        #
+        #     if isinstance(value, str):
+        #         return unit.Quantity(value)
+        #     return value
 
     class PostProcessingParameter(ParametersBase):
-        per_atom_energy: PerAtomEnergy = PerAtomEnergy(
-            normalize=True,
-            from_atom_to_molecule_reduction=True,
-            keep_per_atom_property=True,
-        )
-        general_post_processing_operation: GeneralPostProcessingOperation = (
-            GeneralPostProcessingOperation(
-                calculate_molecular_self_energy=False,
-                calculate_atomic_self_energy=False,
-            )
-        )
+        per_atom_energy: PerAtomEnergy
+        general_post_processing_operation: GeneralPostProcessingOperation
 
     potential_name: str = "ANI2x"
     core_parameter: CoreParameter
@@ -53,26 +65,19 @@ class ANI2xParameters(ParametersBase):
 
 class SchNetParameters(ParametersBase):
     class CoreParameter(ParametersBase):
-        max_Z: int = 101
-        number_of_atom_features: int = 32
-        number_of_radial_basis_functions: int = 20
-        cutoff: str = "5.0 angstrom"
-        number_of_interaction_modules: int = 3
-        number_of_filters: int = 32
-        shared_interactions: bool = False
+        max_Z: int
+        number_of_atom_features: int
+        number_of_radial_basis_functions: int
+        cutoff: Union[str, unit.Quantity]
+        number_of_interaction_modules: int
+        number_of_filters: int
+        shared_interactions: bool
+
+        converted_units = field_validator("cutoff")(convert_str_to_unit)
 
     class PostProcessingParameter(ParametersBase):
-        per_atom_energy: PerAtomEnergy = PerAtomEnergy(
-            normalize=True,
-            from_atom_to_molecule_reduction=True,
-            keep_per_atom_property=True,
-        )
-        general_post_processing_operation: GeneralPostProcessingOperation = (
-            GeneralPostProcessingOperation(
-                calculate_molecular_self_energy=True,
-                calculate_atomic_self_energy=False,
-            )
-        )
+        per_atom_energy: PerAtomEnergy
+        general_post_processing_operation: GeneralPostProcessingOperation
 
     potential_name: str = "SchNet"
     core_parameter: CoreParameter
@@ -81,26 +86,19 @@ class SchNetParameters(ParametersBase):
 
 class PaiNNParameters(ParametersBase):
     class CoreParameter(ParametersBase):
-        max_Z: int = 101
-        number_of_atom_features: int = 32
-        number_of_radial_basis_functions: int = 20
-        cutoff: str = "5.0 angstrom"
-        number_of_interaction_modules: int = 3
-        shared_interactions: bool = False
-        shared_filters: bool = False
+        max_Z: int
+        number_of_atom_features: int
+        number_of_radial_basis_functions: int
+        cutoff: Union[str, unit.Quantity]
+        number_of_interaction_modules: int
+        shared_interactions: bool
+        shared_filters: bool
+
+        converted_units = field_validator("cutoff")(convert_str_to_unit)
 
     class PostProcessingParameter(ParametersBase):
-        per_atom_energy: PerAtomEnergy = PerAtomEnergy(
-            normalize=True,
-            from_atom_to_molecule_reduction=True,
-            keep_per_atom_property=True,
-        )
-        general_post_processing_operation: GeneralPostProcessingOperation = (
-            GeneralPostProcessingOperation(
-                calculate_molecular_self_energy=False,
-                calculate_atomic_self_energy=False,
-            )
-        )
+        per_atom_energy: PerAtomEnergy
+        general_post_processing_operation: GeneralPostProcessingOperation
 
     potential_name: str = "PaiNN"
     core_parameter: CoreParameter
@@ -109,25 +107,18 @@ class PaiNNParameters(ParametersBase):
 
 class PhysNetParameters(ParametersBase):
     class CoreParameter(ParametersBase):
-        max_Z: int = 101
-        number_of_atom_features: int = 64
-        number_of_radial_basis_functions: int = 16
-        cutoff: str = "5.0 angstrom"
-        number_of_interaction_residual: int = 3
-        number_of_modules: int = 5
+        max_Z: int
+        number_of_atom_features: int
+        number_of_radial_basis_functions: int
+        cutoff: Union[str, unit.Quantity]
+        number_of_interaction_residual: int
+        number_of_modules: int
+
+        converted_units = field_validator("cutoff")(convert_str_to_unit)
 
     class PostProcessingParameter(ParametersBase):
-        per_atom_energy: PerAtomEnergy = PerAtomEnergy(
-            normalize=True,
-            from_atom_to_molecule_reduction=True,
-            keep_per_atom_property=True,
-        )
-        general_post_processing_operation: GeneralPostProcessingOperation = (
-            GeneralPostProcessingOperation(
-                calculate_molecular_self_energy=False,
-                calculate_atomic_self_energy=False,
-            )
-        )
+        per_atom_energy: PerAtomEnergy
+        general_post_processing_operation: GeneralPostProcessingOperation
 
     potential_name: str = "PhysNet"
     core_parameter: CoreParameter
@@ -136,25 +127,18 @@ class PhysNetParameters(ParametersBase):
 
 class SAKEParameters(ParametersBase):
     class CoreParameter(ParametersBase):
-        max_Z: int = 101
-        number_of_atom_features: int = 64
-        number_of_radial_basis_functions: int = 50
-        cutoff: str = "5.0 angstrom"
-        number_of_interaction_modules: int = 6
-        number_of_spatial_attention_heads: int = 4
+        max_Z: int
+        number_of_atom_features: int
+        number_of_radial_basis_functions: int
+        cutoff: Union[str, unit.Quantity]
+        number_of_interaction_modules: int
+        number_of_spatial_attention_heads: int
+
+        converted_units = field_validator("cutoff")(convert_str_to_unit)
 
     class PostProcessingParameter(ParametersBase):
-        per_atom_energy: PerAtomEnergy = PerAtomEnergy(
-            normalize=True,
-            from_atom_to_molecule_reduction=True,
-            keep_per_atom_property=True,
-        )
-        general_post_processing_operation: GeneralPostProcessingOperation = (
-            GeneralPostProcessingOperation(
-                calculate_molecular_self_energy=False,
-                calculate_atomic_self_energy=False,
-            )
-        )
+        per_atom_energy: PerAtomEnergy
+        general_post_processing_operation: GeneralPostProcessingOperation
 
     potential_name: str = "SAKE"
     core_parameter: CoreParameter

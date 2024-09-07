d11efe794d2d3abad4dc356cdcceb431bae99668
MarshallYan <yanshuai001220@gmail.com>
2024-09-07 00:07:27 -0400

    change highest_atomic_number to maximum_atomic number for code consistency with other NNPs

--- modelforge/potential/parameters.py
+++ modelforge/potential/parameters.py
@@ -199,7 +199,7 @@ class TensorNetParameters(ParametersBase):
         number_of_radial_basis_functions: int
         maximum_interaction_radius: Union[str, unit.Quantity]
         minimum_interaction_radius: Union[str, unit.Quantity]
-        highest_atomic_number: int
+        maximum_atomic_number: int
         equivariance_invariance_group: str
         activation_function_parameter: ActivationFunctionConfig
 

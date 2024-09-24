45672e993f216583bdfd66fa9f1a92c4804c1643
wiederm <marcus.wieder@gmail.com>
2024-09-24 16:00:45 +0200

    small refactoring of dataset structure

--- modelforge/train/training.py
+++ modelforge/train/training.py
@@ -245,7 +245,7 @@ class Loss(nn.Module):
         "per_atom_force",
     ]
 
-    def __init__(self, loss_porperty: List[str], weights: Dict[str, float]):
+    def __init__(self, loss_property: List[str], weights: Dict[str, float]):
         """
         Calculates the combined loss for energy and force predictions.
 
@@ -264,11 +264,11 @@ class Loss(nn.Module):
         super().__init__()
         from torch.nn import ModuleDict
 
-        self.loss_property = loss_porperty
+        self.loss_property = loss_property
         self.weights = weights
         self.loss_functions = ModuleDict()
 
-        for prop in loss_porperty:
+        for prop in loss_property:
             if prop not in self._SUPPORTED_PROPERTIES:
                 raise NotImplementedError(f"Loss type {prop} not implemented.")
 
@@ -278,9 +278,13 @@ class Loss(nn.Module):
                 )
 
             if prop == "per_atom_energy":
-                self.loss_functions[prop] = PerMoleculeSquaredError(scale_by_number_of_atoms=True)
+                self.loss_functions[prop] = PerMoleculeSquaredError(
+                    scale_by_number_of_atoms=True
+                )
             if prop == "per_molecule_energy":
-                self.loss_functions[prop] = PerMoleculeSquaredError(scale_by_number_of_atoms=True)
+                self.loss_functions[prop] = PerMoleculeSquaredError(
+                    scale_by_number_of_atoms=True
+                )
 
             self.register_buffer(prop, torch.tensor(self.weights[prop]))
 
@@ -541,7 +545,7 @@ class TrainingAdapter(pL.LightningModule):
 
         Parameters
         ----------
-        potential_parameter : Union[ANI2xParameters, SAKEParameters, SchNetParameters, PhysNetParameters, PaiNNParameters, TensorNetParameters]
+        potential_parameter : T_NNP_Parameters
             Parameters for the potential model.
         dataset_statistic : Dict[str, float]
             The statistics of the dataset, such as mean and standard deviation.
@@ -571,7 +575,7 @@ class TrainingAdapter(pL.LightningModule):
         self.calculate_predictions = CalculateProperties(
             training_parameter.loss_parameter.loss_property
         )
-        self.optimizer = training_parameter.optimizer
+        self.optimizer_class = training_parameter.optimizer
         self.learning_rate = training_parameter.lr
         self.lr_scheduler = training_parameter.lr_scheduler
 
@@ -599,7 +603,7 @@ class TrainingAdapter(pL.LightningModule):
             training_parameter.loss_parameter.loss_property
         )
 
-        self.loss_metric = create_error_metrics(
+        self.loss_metrics = create_error_metrics(
             training_parameter.loss_parameter.loss_property, is_loss=True
         )
 
@@ -682,12 +686,9 @@ class TrainingAdapter(pL.LightningModule):
 
         # Update loss metrics with per-sample losses
         for key, metric in loss_dict.items():
-            self.loss_metric[key].update(metric.detach(), batch_size=batch.batch_size())
-
-        # Update total loss metric # FIXME: check if the following is necessary
-        # self.loss_metrics["total_loss"].update(
-        #     loss_dict["total_loss"].detach(), batch_size=loss_dict["total_loss"].size(0)
-        # )
+            self.loss_metrics[key].update(
+                metric.detach(), batch_size=batch.batch_size()
+            )
 
         # Return the mean total loss for optimization
         return mean_total_loss
@@ -748,23 +749,49 @@ class TrainingAdapter(pL.LightningModule):
         # Update and log metrics
         self._update_metrics(self.test_metrics, predict_target)
 
-    def on_test_epoch_end(self):
-        """
-        Operations to perform at the end of the test set pass.
+    def on_validation_epoch_end(self):
+        """Logs metrics at the end of the validation epoch."""
+        self._log_metrics(self.val_metrics, "val")
 
-        This method is automatically called by PyTorch Lightning at the end of
-        the test epoch. It logs the accumulated metrics for the test phase.
-        """
-        self._log_on_epoch(log_mode="test")
+    def on_test_epoch_end(self):
+        """Logs metrics at the end of the test epoch."""
+        self._log_metrics(self.test_metrics, "test")
 
     def on_train_epoch_end(self):
-        """
-        Operations to perform at the end of each training epoch.
+        """Logs metrics at the end of the training epoch."""
+        self._log_metrics(self.train_metrics, "train")
+        self._log_metrics(self.loss_metrics, "loss")
+        self._log_learning_rate()
+        self._log_histograms()
 
-        This method is automatically called by PyTorch Lightning at the end of
-        each training epoch. It logs histograms of weights and biases, learning
-        rate, and resets validation loss.
-        """
+    def _log_learning_rate(self):
+        """Logs the current learning rate."""
+        sch = self.lr_schedulers()
+        try:
+            self.log("lr", sch.get_last_lr()[0], on_epoch=True, prog_bar=True)
+        except AttributeError:
+            pass
+
+    def _log_metrics(self, metrics: ModuleDict, phase: str):
+        """Logs all accumulated metrics for a given phase."""
+        # abbreviate long names to shorter versions
+        abbreviate = {
+            "MeanAbsoluteError": "mae",
+            "MeanSquaredError": "rmse",
+            "MeanMetric": "mse",  # NOTE: MeanMetric is the MSE since we accumulate the squared error
+        }  # NOTE: MeanSquaredError(squared=False) is RMSE
+
+        for prop, metric_collection in metrics.items():
+            for metric_name, metric in metric_collection.items():
+                metric_value = metric.compute()
+                metric.reset()
+                self.log(
+                    f"{phase}/{prop}/{abbreviate[metric_name]}",
+                    metric_value,
+                    prog_bar=True,
+                )
+
+    def _log_histograms(self):
         if self.log_histograms == True:
             for name, params in self.named_parameters():
                 if params is not None:
@@ -776,70 +803,14 @@ class TrainingAdapter(pL.LightningModule):
                         f"{name}.grad", params.grad, self.current_epoch
                     )
 
-        sch = self.lr_schedulers()
-        try:
-            self.log("lr", sch.get_last_lr()[0], on_epoch=True, prog_bar=True)
-        except AttributeError:
-            pass
-
-        self._log_on_epoch()
-
-    def _log_on_epoch(self, log_mode: str = "train"):
-        """
-        Logs all accumulated metrics at the end of an epoch.
-
-        Parameters
-        ----------
-        log_mode : str, optional
-            The phase of training for which metrics are being logged, by default "train".
-            It can be "train", "val", or "test".
-        """
-        # convert long names to shorter versions
-        conv = {
-            "MeanAbsoluteError": "mae",
-            "MeanSquaredError": "rmse",
-            "MeanMetric": "mse",  # NOTE: MeanMetric is the MSE since we accumulate the squared error
-        }  # NOTE: MeanSquaredError(squared=False) is RMSE
-
-        # Log all accumulated metrics for train and val phases
-        if log_mode == "train":
-            errors = [
-                ("train", self.train_metrics),
-                ("val", self.val_metrics),
-                ("loss", self.loss_metric),
-            ]
-        elif log_mode == "test":
-            errors = [
-                ("test", self.test_metrics),
-            ]
-        else:
-            raise RuntimeError(f"Unrecognized mode: {log_mode}")
-
-        for phase, error_dict in errors:
-            # skip if log_on_training_step is not requested
-            if phase == "train" and not self.log_on_training_step:
-                continue
-
-            for property, metrics_dict in error_dict.items():
-                for name, metric in metrics_dict.items():
-                    name = f"{phase}/{property}/{conv.get(name, name)}"
-                    self.log(name, metric.compute(), prog_bar=True, sync_dist=True)
-                    metric.reset()
-
     def configure_optimizers(self):
-        """
-        Configures the model's optimizers (and optionally schedulers).
+        """Configures the optimizers and learning rate schedulers."""
 
-        Returns
-        -------
-        Dict[str, Any]
-            A dictionary containing the optimizer and optionally the learning rate scheduler
-            to be used within the PyTorch Lightning training process.
-        """
+        optimizer = self.optimizer_class(
+            self.potential.parameters(), lr=self.learning_rate
+        )
 
-        optimizer = self.optimizer(self.potential.parameters(), lr=self.learning_rate)
-
-        lr_scheduler = self.lr_scheduler.model_dump().copy()
+        lr_scheduler = self.lr_scheduler.model_dump()
         interval = lr_scheduler.pop("interval")
         frequency = lr_scheduler.pop("frequency")
         monitor = lr_scheduler.pop("monitor")
@@ -849,13 +820,13 @@ class TrainingAdapter(pL.LightningModule):
             **lr_scheduler,
         )
 
-        lr_scheduler = {
+        scheduler = {
             "scheduler": lr_scheduler,
             "monitor": monitor,  # Name of the metric to monitor
             "interval": interval,
             "frequency": frequency,
         }
-        return {"optimizer": optimizer, "lr_scheduler": lr_scheduler}
+        return {"optimizer": optimizer, "lr_scheduler": scheduler}
 
 
 from openff.units import unit
@@ -875,7 +846,7 @@ class ModelTrainer:
         runtime_parameter: RuntimeParameters,
         dataset_statistic: Dict[str, Dict[str, unit.Quantity]],
         use_default_dataset_statistic: bool,
-        optimizer: Type[Optimizer] = torch.optim.AdamW,
+        optimizer_class: Type[Optimizer] = torch.optim.AdamW,
         potential_seed: Optional[int] = None,
         verbose: bool = False,
     ):
@@ -923,7 +894,7 @@ class ModelTrainer:
         self.model = self.setup_potential(potential_seed)
         self.callbacks = self.setup_callbacks()
         self.trainer = self.setup_trainer()
-        self.optimizer = optimizer
+        self.optimizer_class = optimizer_class
         self.learning_rate = self.training_parameter.lr
         self.lr_scheduler = self.training_parameter.lr_scheduler
 
@@ -1048,15 +1019,20 @@ class ModelTrainer:
         pL.loggers.Logger
             Configured logger instance.
         """
+
+        experiment_name = self._format_experiment_name(
+            self.runtime_parameter.experiment_name
+        )
+
         if self.training_parameter.experiment_logger.logger_name == "tensorboard":
             from lightning.pytorch.loggers import TensorBoardLogger
 
-            logger = TensorBoardLogger(
-                save_dir=str(
-                    self.training_parameter.experiment_logger.tensorboard_configuration.save_dir
-                ),  # FIXME: same variable for all logger, maybe we can use a varable not bound to a logger for this?
-                name=self._replace_placeholder_in_experimental_name(
-                    self.runtime_parameter.experiment_name
+            logger = (
+                TensorBoardLogger(
+                    save_dir=str(
+                        self.training_parameter.experiment_logger.tensorboard_configuration.save_dir
+                    ),
+                    name=experiment_name,
                 ),
             )
         elif self.training_parameter.experiment_logger.logger_name == "wandb":
@@ -1075,14 +1051,14 @@ class ModelTrainer:
                 project=self.training_parameter.experiment_logger.wandb_configuration.project,
                 group=self.training_parameter.experiment_logger.wandb_configuration.group,
                 job_type=self.training_parameter.experiment_logger.wandb_configuration.job_type,
-                tags=self._add_tags(
+                tags=self._generate_tags(
                     self.training_parameter.experiment_logger.wandb_configuration.tags
                 ),
                 notes=self.training_parameter.experiment_logger.wandb_configuration.notes,
-                name=self._replace_placeholder_in_experimental_name(
-                    self.runtime_parameter.experiment_name
-                ),
+                name=experiment_name,
             )
+        else:
+            raise ValueError("Unsupported logger type.")
         return logger
 
     def setup_callbacks(self) -> List[Any]:
@@ -1101,14 +1077,14 @@ class ModelTrainer:
         )
 
         callbacks = []
-        if self.training_parameter.stochastic_weight_averaging is not None:
+        if self.training_parameter.stochastic_weight_averaging:
             callbacks.append(
                 StochasticWeightAveraging(
                     **self.training_parameter.stochastic_weight_averaging.model_dump()
                 )
             )
 
-        if self.training_parameter.early_stopping is not None:
+        if self.training_parameter.early_stopping:
             callbacks.append(
                 EarlyStopping(**self.training_parameter.early_stopping.model_dump())
             )
@@ -1117,12 +1093,13 @@ class ModelTrainer:
             f"best_{self.potential_parameter.potential_name}-{self.dataset_parameter.dataset_name}"
             + "-{epoch:02d}-{val_loss:.2f}"
         )
-        checkpoint_callback = ModelCheckpoint(
-            save_top_k=2,
-            monitor=self.training_parameter.monitor_for_checkpoint,
-            filename=checkpoint_filename,
+        callbacks.append(
+            ModelCheckpoint(
+                save_top_k=2,
+                monitor=self.training_parameter.monitor_for_checkpoint,
+                filename=checkpoint_filename,
+            )
         )
-        callbacks.append(checkpoint_callback)
         return callbacks
 
     def setup_trainer(self) -> Trainer:
@@ -1187,12 +1164,14 @@ class ModelTrainer:
         )
 
         self.trainer.validate(
+            model=self.model,
             dataloaders=self.datamodule.val_dataloader(),
             ckpt_path="best",
             verbose=True,
         )
 
         self.trainer.test(
+            model=self.model,
             dataloaders=self.datamodule.test_dataloader(),
             ckpt_path="best",
             verbose=True,
@@ -1209,7 +1188,7 @@ class ModelTrainer:
         log.warning("Model does not implement _config_prior().")
         raise NotImplementedError()
 
-    def _replace_placeholder_in_experimental_name(self, experiment_name: str) -> str:
+    def _format_experiment_name(self, experiment_name: str) -> str:
         """
         Replace the placeholders in the experiment name with the actual values.
 
@@ -1232,35 +1211,18 @@ class ModelTrainer:
         )
         return experiment_name
 
-    def _add_tags(self, tags: List[str]) -> List[str]:
-        """
-        Add tags to the wandb tags.
-
-        Parameters
-        ----------
-        tags : List[str]
-            List of tags to add to the experiment.
-
-        Returns
-        -------
-        List[str]
-            List of tags for the experiment.
-        """
-
-        # add version
+    def _generate_tags(self, tags: List[str]) -> List[str]:
+        """Generates tags for the experiment."""
         import modelforge
 
-        tags.append(str(modelforge.__version__))
-        # add dataset
-        tags.append(self.dataset_parameter.dataset_name)
-        # add potential name
-        tags.append(self.potential_parameter.potential_name)
-        # add information about what is included in the loss
-        str_loss_property = "-".join(
-            self.training_parameter.loss_parameter.loss_property
+        tags.extend(
+            [
+                str(modelforge.__version__),
+                self.dataset_parameter.dataset_name,
+                self.potential_parameter.potential_name,
+                f"loss-{'-'.join(self.training_parameter.loss_parameter.loss_property)}",
+            ]
         )
-        tags.append(f"loss-{str_loss_property}")
-
         return tags
 
 

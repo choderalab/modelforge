bugfix (we need to retain graph if we want to calculate high order derivatives)

modelforge/potential/models.py
@@ -11,7 +11,7 @@ from openff.units import unit
 from torch.nn import Module
 from modelforge.potential.neighbors import PairlistData
 
-from modelforge.dataset.dataset import DatasetParameters, NNPInput, NNPInputTuple
+from modelforge.dataset.dataset import DatasetParameters, NNPInputTuple
 from modelforge.potential.parameters import (
     AimNet2Parameters,
     ANI2xParameters,

modelforge/train/training.py
@@ -127,7 +127,7 @@ class CalculateProperties(torch.nn.Module):
             total_energy,
             nnp_input.positions,
             create_graph=train_mode,
-            retain_graph=False,
+            retain_graph=train_mode,
             allow_unused=False,
         )[0]
 
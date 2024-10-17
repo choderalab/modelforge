da447a3357b27c905455b545c74cbf6ddaaf9713
wiederm <marcus.wieder@univie.ac.at>
2024-10-17 20:02:29 +0200

    log the gradient norm of the loss components

--- modelforge/train/training.py
+++ modelforge/train/training.py
@@ -53,6 +53,19 @@ def gradient_norm(model):
     total_norm = total_norm ** (1.0 / 2)
     return total_norm
 
+def compute_grad_norm(loss, model):
+    parameters = [p for p in model.parameters() if p.requires_grad]
+    grads = torch.autograd.grad(loss.sum(), parameters, retain_graph=True, create_graph=False, allow_unused=True,)
+    total_norm = 0.0
+    for grad in grads:
+        if grad is not None:
+            param_norm = grad.detach().data.norm(2)
+            total_norm += param_norm.item() ** 2
+    total_norm = total_norm ** 0.5
+    return total_norm
+
+
+
 
 def _exchange_per_atom_energy_for_per_molecule_energy(prop: str) -> str:
     """
@@ -487,11 +500,16 @@ class TrainingAdapter(pL.LightningModule):
         batch_size = batch.batch_size()
         for key, metric in loss_dict.items():
             self.loss_metrics[key].update(metric.detach(), batch_size=batch_size)
+            # Compute and log gradient norms for each loss component
+            if key == 'total_loss':
+                continue
+            grad_norm = compute_grad_norm(metric.mean(), self)
+            self.log(f"grad_norm/{key}", grad_norm)
 
         # Compute the mean loss for optimization
-        mean_total_loss = loss_dict["total_loss"].mean()
+        total_loss = loss_dict["total_loss"].mean()
 
-        return mean_total_loss
+        return total_loss
 
     def on_after_backward(self):
         # After backward pass
@@ -872,8 +890,8 @@ class ModelTrainer:
             Logs the gradient norm.
             """
 
-            def on_before_optimizer_step(self, trainer, model):
-                model.log("my_model/grad_norm", gradient_norm(model))
+            def on_before_optimizer_step(self, trainer, pl_module, optimizer):
+                pl_module.log("model/grad_norm", gradient_norm(pl_module))
 
         callbacks.append(GradNormCallback())
 

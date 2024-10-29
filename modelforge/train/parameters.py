1eba4ff5fcc1329c84db3aa4d1bfe014e540b8de
wiederm <marcus.wieder@gmail.com>
2024-10-29 14:30:34 +0100

    linter again

--- modelforge/train/parameters.py
+++ modelforge/train/parameters.py
@@ -378,7 +378,7 @@ class TrainingParameters(ParametersBase):
     shift_center_of_mass_to_origin: bool
     batch_size: int
     lr: float
-    plot_frequency: int = 5 # how often to log regression and error histograms 
+    plot_frequency: int = 5  # how often to log regression and error histograms
     lr_scheduler: Optional[SchedulerConfig] = None
     loss_parameter: LossParameter
     early_stopping: Optional[EarlyStopping] = None

c74114e039d8fada1be34894cbd3d5e8a2f4b02d
wiederm <marcus.wieder@gmail.com>
2024-11-05 11:38:22 +0100

    add internal aimnet2 standard test

--- modelforge/tests/test_aimnet2.py
+++ modelforge/tests/test_aimnet2.py
@@ -141,12 +141,85 @@ def test_radial_symmetry_function_regression():
 def test_forward(single_batch_with_batchsize, prep_temp_dir):
     """Test initialization of the AIMNet2 model."""
     # read default parameters
-    aimnet = setup_potential_for_test("aimnet2", "training")
+    aimnet = setup_potential_for_test("aimnet2", "training", potential_seed=42)
 
     assert aimnet is not None, "Aimnet model should be initialized."
     batch = single_batch_with_batchsize(64, "QM9", str(prep_temp_dir))
 
     y_hat = aimnet(batch.nnp_input)
+
+    assert y_hat is not None, "Aimnet model should be able to make predictions."
+
+    ref_per_system_energy = torch.tensor(
+        [
+            [-1.6222e00],
+            [-1.7771e-01],
+            [1.5974e-01],
+            [-1.2089e-02],
+            [-1.8864e-01],
+            [-2.7185e-01],
+            [-4.3214e00],
+            [-1.3357e00],
+            [-1.1657e00],
+            [-1.4146e00],
+            [-1.8898e00],
+            [-1.1582e00],
+            [-9.1212e00],
+            [-4.8285e00],
+            [-5.0907e00],
+            [-5.4467e00],
+            [-1.8100e00],
+            [-4.9845e00],
+            [-3.7676e00],
+            [-2.5988e00],
+            [-1.5824e01],
+            [-1.0948e01],
+            [-2.8324e-01],
+            [-4.5179e-01],
+            [-6.8437e-01],
+            [-3.1547e-01],
+            [-5.7387e-01],
+            [-4.6788e-01],
+            [-1.9818e00],
+            [-3.8900e00],
+            [-4.2745e00],
+            [-2.8107e00],
+            [-1.2960e00],
+            [-1.5892e00],
+            [-5.7663e00],
+            [-4.2937e00],
+            [-3.0977e00],
+            [-2.2906e00],
+            [-1.4034e01],
+            [-9.6701e00],
+            [-7.9657e00],
+            [-6.4762e00],
+            [-9.7999e00],
+            [-5.6619e00],
+            [-9.1679e00],
+            [-6.8304e00],
+            [-1.0582e01],
+            [-6.0419e00],
+            [-7.2018e00],
+            [-5.0521e00],
+            [-4.0748e00],
+            [-3.5285e00],
+            [-2.5017e00],
+            [-2.5237e01],
+            [-1.9461e01],
+            [-1.7413e00],
+            [-2.1273e00],
+            [-2.5887e00],
+            [-1.1963e00],
+            [-2.4938e00],
+            [-3.1271e00],
+            [-1.7812e00],
+            [-8.0866e00],
+            [-8.7542e00],
+        ],
+    )
+
+    assert torch.allclose(y_hat['per_system_energy'], ref_per_system_energy, atol=1e-3)
 
 
 @pytest.mark.xfail(raises=NotImplementedError)

2809acd79b187b70612806cf65c2c9cf1336d113
chrisiacovella <chris.iacovella@gmail.com>
2024-12-26 14:07:28 -0800

    fixing linting complaints.

--- modelforge/utils/prop.py
+++ modelforge/utils/prop.py
@@ -74,7 +74,6 @@ class NNPInput:
         # Validate inputs
         self._validate_inputs()
 
-
     def _validate_inputs(self):
         # Get shapes of the arrays
         atomic_numbers_shape = self.atomic_numbers.shape
@@ -187,6 +186,7 @@ class Metadata:
         return self
 
     def to_dtype(self, dtype: torch.dtype):
+        """Move all **relevant** tensors to specified dtype."""
         self.per_system_energy = self.per_system_energy.to(dtype)
         self.per_atom_force = self.per_atom_force.to(dtype)
         self.per_system_dipole_moment = self.per_system_dipole_moment.to(dtype)

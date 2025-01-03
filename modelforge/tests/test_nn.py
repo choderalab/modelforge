154ba66a5e71734008127777e9edc402225ec7c3
chrisiacovella <chris.iacovella@gmail.com>
2025-01-02 16:03:46 -0800

    merged featurization tests into test_nn.py; added extra tests for better coverage.

--- modelforge/tests/test_nn.py
+++ modelforge/tests/test_nn.py
@@ -72,3 +72,144 @@ def test_embedding(single_batch_with_batchsize, prep_temp_dir):
     assert out.shape == torch.Size(
         [557, 32]
     )  # nr_of_atoms, nr_of_per_atom_features (the total charge is mixed in)
+
+
+def test_add_per_molecule_value(prep_temp_dir, single_batch_with_batchsize):
+    from modelforge.potential.featurization import AddPerMoleculeValue
+
+    temp_prop = AddPerMoleculeValue(key="per_system_total_charge")
+    assert temp_prop.key == "per_system_total_charge"
+
+    batch = single_batch_with_batchsize(
+        batch_size=1,
+        dataset_name="SPICE2",
+        local_cache_dir=str(prep_temp_dir),
+        version_select="nc_1000_v0",
+    )
+
+    number_of_per_atom_features = 10
+    atomic_number_embedding = torch.nn.Embedding(100, number_of_per_atom_features)
+    atomic_numbers = batch.nnp_input.atomic_numbers
+
+    embedding_tensor = atomic_number_embedding(atomic_numbers)
+    assert embedding_tensor.shape[0] == atomic_numbers.shape[0]
+    assert embedding_tensor.shape[1] == number_of_per_atom_features
+
+    embedding_tensor = temp_prop(embedding_tensor, batch.nnp_input)
+
+    assert embedding_tensor.shape[0] == atomic_numbers.shape[0]
+    assert embedding_tensor.shape[1] == number_of_per_atom_features + 1
+
+
+def test_add_per_atom_value(prep_temp_dir, single_batch_with_batchsize):
+    from modelforge.potential.featurization import AddPerAtomValue
+
+    temp_prop = AddPerAtomValue(key="per_atom_partial_charge")
+    assert temp_prop.key == "per_atom_partial_charge"
+
+    batch = single_batch_with_batchsize(
+        batch_size=1,
+        dataset_name="SPICE2",
+        local_cache_dir=str(prep_temp_dir),
+        version_select="nc_1000_v0",
+    )
+    # we need to set the per_atom_partial_charge to a tensor of some values
+    # because by default we don't load in the values from the spice dataset
+    batch.nnp_input.per_atom_partial_charge = torch.zeros(
+        batch.nnp_input.atomic_numbers.shape[0], 1
+    )
+
+    number_of_per_atom_features = 10
+    atomic_number_embedding = torch.nn.Embedding(100, number_of_per_atom_features)
+    atomic_numbers = batch.nnp_input.atomic_numbers
+
+    embedding_tensor = atomic_number_embedding(atomic_numbers)
+    assert embedding_tensor.shape[0] == atomic_numbers.shape[0]
+    assert embedding_tensor.shape[1] == number_of_per_atom_features
+
+    embedding_tensor = temp_prop(embedding_tensor, batch.nnp_input)
+
+    assert embedding_tensor.shape[0] == atomic_numbers.shape[0]
+    assert embedding_tensor.shape[1] == number_of_per_atom_features + 1
+
+
+def test_group_and_period_embedding(prep_temp_dir, single_batch_with_batchsize):
+    batch = single_batch_with_batchsize(
+        batch_size=1,
+        dataset_name="tmqm",
+        local_cache_dir=str(prep_temp_dir),
+        version_select="nc_1000_v0",
+    )
+
+    number_of_per_atom_features = 10
+    atomic_number_embedding = torch.nn.Embedding(100, number_of_per_atom_features)
+    atomic_numbers = batch.nnp_input.atomic_numbers
+
+    embedding_tensor = atomic_number_embedding(atomic_numbers)
+
+    from modelforge.potential.featurization import GroupPeriodEmbedding
+
+    maximum_period = 7
+    number_of_per_period_features = 5
+    temp_period_embedding_tensor = torch.nn.Embedding(
+        maximum_period, number_of_per_period_features
+    )
+    temp_prop = GroupPeriodEmbedding(temp_period_embedding_tensor, key="atomic_periods")
+    assert temp_prop.key == "atomic_periods"
+    assert temp_prop.embedding_tensor == temp_period_embedding_tensor
+
+    embedding_tensor = temp_prop(embedding_tensor, batch.nnp_input)
+
+    assert embedding_tensor.shape[0] == atomic_numbers.shape[0]
+    assert (
+        embedding_tensor.shape[1]
+        == number_of_per_atom_features + number_of_per_period_features
+    )
+
+    maximum_group = 18
+    number_of_per_group_features = 11
+    temp_group_embedding_tensor = torch.nn.Embedding(
+        maximum_group, number_of_per_group_features
+    )
+    temp_prop2 = GroupPeriodEmbedding(temp_group_embedding_tensor, key="atomic_groups")
+    assert temp_prop2.key == "atomic_groups"
+
+    embedding_tensor = temp_prop2(embedding_tensor, batch.nnp_input)
+    assert embedding_tensor.shape[0] == atomic_numbers.shape[0]
+    assert (
+        embedding_tensor.shape[1]
+        == number_of_per_atom_features
+        + number_of_per_period_features
+        + number_of_per_group_features
+    )
+
+
+def test_featurization_input(prep_temp_dir, single_batch_with_batchsize):
+    # less exhaustive test, than test_embedding, but should be sufficient
+    # to make sure that atomic period and group embedding can be setup
+    from modelforge.potential.featurization import FeaturizeInput
+
+    featurization_dict = {
+        "properties_to_featurize": ["atomic_number", "atomic_period", "atomic_group"],
+        "atomic_number": {
+            "maximum_atomic_number": 101,
+            "number_of_per_atom_features": 32,
+        },
+        "atomic_period": {
+            "maximum_period": 8,
+            "number_of_per_period_features": 5,
+        },
+        "atomic_group": {
+            "maximum_group": 19,
+            "number_of_per_group_features": 11,
+        },
+    }
+
+    temp_features = FeaturizeInput(featurization_dict)
+
+    assert "atomic_number" in temp_features.registered_embedding_operations
+    assert "atomic_period" in temp_features.registered_embedding_operations
+    assert "atomic_group" in temp_features.registered_embedding_operations
+
+    assert len(temp_features.registered_embedding_operations) == 3
+    assert len(temp_features.embeddings) == 2

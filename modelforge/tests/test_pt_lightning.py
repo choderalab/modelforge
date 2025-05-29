9ae200dbee21b8946fe99b585fd20965a9fb4fb1
chrisiacovella <chris.iacovella@gmail.com>
2025-05-29 09:41:10 -0700

    fixing lightning test...some how missed this.

--- modelforge/tests/test_pt_lightning.py
+++ modelforge/tests/test_pt_lightning.py
@@ -13,9 +13,17 @@ def test_datamodule(prep_temp_dir, dataset_temp_dir):
 
     # Set up dataset
 
+    properties_of_interest = ["atomic_numbers", "positions", "internal_energy_at_0K"]
+    properties_assignment = {
+        "atomic_numbers": "atomic_numbers",
+        "positions": "positions",
+        "E": "internal_energy_at_0K",
+    }
     dm = DataModule(
         name="QM9",
         batch_size=512,
         local_cache_dir=str(prep_temp_dir),
         dataset_cache_dir=str(dataset_temp_dir),
+        properties_of_interest=properties_of_interest,
+        properties_assignment=properties_assignment,
     )

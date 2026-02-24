9d2650c87019ef9a5f3a65068071b59eda282e8a
Shuai Yan <yanshuai001220@gmail.com>
2026-02-24 10:21:15 -0600

    Fix string quotes for multiprocessing context

--- modelforge/dataset/dataset.py
+++ modelforge/dataset/dataset.py
@@ -1597,7 +1597,7 @@ class DataModule(pl.LightningDataModule):
                 shuffle=False,
                 pin_memory=False,
                 persistent_workers=False,
-                multiprocessing_context='fork',
+                multiprocessing_context="fork",
             ),
             desc="Calculating pairlist for dataset",
         ):
@@ -1634,7 +1634,7 @@ class DataModule(pl.LightningDataModule):
             num_workers=num_workers,
             shuffle=shuffle,
             pin_memory=pin_memory,
-            multiprocessing_context='fork',
+            multiprocessing_context="fork",
         )
 
     def val_dataloader(self, num_workers: int = 4) -> DataLoader:
@@ -1651,7 +1651,7 @@ class DataModule(pl.LightningDataModule):
             batch_size=self.batch_size,
             collate_fn=collate_conformers,
             num_workers=num_workers,
-            multiprocessing_context='fork',
+            multiprocessing_context="fork",
         )
 
     def test_dataloader(self, num_workers: int = 4) -> DataLoader:
@@ -1668,7 +1668,7 @@ class DataModule(pl.LightningDataModule):
             batch_size=self.batch_size,
             collate_fn=collate_conformers,
             num_workers=num_workers,
-            multiprocessing_context='fork',
+            multiprocessing_context="fork",
         )
 
 
